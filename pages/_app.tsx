b423e3f1d7a35a6e94f7ab3030b8f8fa8f7c6827
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-23 21:49:34 +0100

    initial commit of _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from 'next/app';
+
+import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            </Head>
+            <main id="main">
+                <Component {...pageProps} />
+            </main>
+        </>
+    )
 }
-export default MyApp
+
+export default MyApp;
