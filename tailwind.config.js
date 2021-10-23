c5bc7653356c8e5208ad4df8c04b219e3a2b7042
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-23 21:46:06 +0100

    tailwindcss init

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['pages/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
+    darkMode: false, // or 'media' or 'class',
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
