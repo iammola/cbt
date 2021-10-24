1ba0467a32678a78b84768958db7379951a2c580
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-24 13:10:59 +0100

    initial commit of classNames function to generate class name strings

---
+++ utils/index.ts
@@ -0,0 +1,6 @@
+export function classNames(...arg: (string | string[] | { [key: string]: any } | null | undefined)[]): string {
+    return [arg.filter(item => Array.isArray(item) || typeof item === "string") as (string | string[])[], (arg.filter(item => !Array.isArray(item) && typeof item === "object") as { [key: string]: any }[]).reduce((acc: string[], obj: any) => {
+        acc.push(Object.keys(Object.fromEntries(Object.entries(obj ?? {}).filter(i => Boolean(i[1]) === true))).join(' '));
+        return acc;
+    }, [])].flat().join(' ');
+}
