99bd009241fbe3b4f1a9c2df99a58eb623f034c6
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-24 13:10:12 +0100

    built registration form

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,14 +1,94 @@
+import useSWR from "swr";
 import { NextPage } from "next";
+import { format } from "date-fns";
 import { Fragment, useEffect, useState, FunctionComponent } from 'react';
 
 import { Listbox, Transition } from "@headlessui/react";
+import { CalendarIcon, CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+
 import type { ClassRecord } from "db/models/Class";
 
 type SelectOption = Pick<ClassRecord<true>, '_id' | 'name'>;
 
 const Register: NextPage = () => {
+    const [subjects, setSubjects] = useState<SelectOption[] | undefined>();
+    const { data: classes, error } = useSWR<SelectOption[], true>('/api/classes?select=name', url => fetch(url).then(res => res.json()));
+
+    const [selectedDate, setSelectedDate] = useState<number | undefined>();
+    const [selectedDuration, setSelectedDuration] = useState<number | undefined>();
+    const [selectedClass, setSelectedClass] = useState<SelectOption>({ _id: "", name: "Loading classes..." });
+    const [selectedSubject, setSelectedSubject] = useState<SelectOption>({ _id: "", name: "Select class first" });
     return (
         <section className="flex flex-col">
+            <section className="flex flex-col items-center justify-center w-screen h-screen">
+                <section className="flex flex-col gap-5 rounded-2xl shadow-lg p-10 bg-gray-50">
+                    <h2 className="text-2xl text-gray-800 text-center tracking-white font-extrabold">
+                        Registration
+                    </h2>
+                    <form className="flex flex-col gap-3 w-full">
+                        <Select
+                            label="Classes"
+                            options={classes}
+                            selected={selectedClass}
+                            handleChange={setSelectedClass}
+                        />
+                        <Select
+                            label="Subjects"
+                            options={subjects}
+                            selected={selectedSubject}
+                            handleChange={setSelectedSubject}
+                        />
+                        <div className="flex flex-col w-80 relative">
+                            <label
+                                htmlFor="date"
+                                className="font-semibold text-sm text-gray-700"
+                            >
+                                Date
+                            </label>
+                            <input
+                                required
+                                id="date"
+                                type="date"
+                                onChange={({ target: { valueAsNumber } }) => setSelectedDate(valueAsNumber)}
+                                value={selectedDate !== undefined ? format(new Date(selectedDate), 'yyyy-MM-dd') : ''}
+                                className="w-full mt-2 py-3.5 pl-3 pr-10 text-left sm:text-sm bg-white rounded-lg shadow-md transition-shadow focus:ring-2 focus:ring-green-400 focus:outline-none"
+                            />
+                            <span className="absolute bottom-4 right-0 z-10 flex items-center pr-2 pointer-events-none text-gray-400">
+                                <CalendarIcon
+                                    className="w-5 h-5"
+                                    aria-hidden="true"
+                                />
+                            </span>
+                        </div>
+                        <div className="flex flex-col w-80 relative">
+                            <label
+                                htmlFor="duration"
+                                className="font-semibold text-sm text-gray-700"
+                            >
+                                Duration
+                            </label>
+                            <input
+                                required
+                                id="duration"
+                                type="number"
+                                value={selectedDuration ?? ''}
+                                onChange={({ target: { valueAsNumber } }) => setSelectedDuration(valueAsNumber)}
+                                className="w-full mt-2 py-3.5 pl-3 pr-10 text-left sm:text-sm bg-white rounded-lg shadow-md transition-shadow focus:ring-2 focus:ring-green-400 focus:outline-none"
+                            />
+                            <span className="absolute bottom-4 right-0 z-10 flex items-center pr-2 pointer-events-none text-gray-400 text-sm">
+                                in mins
+                            </span>
+                        </div>
+                        <button
+                            type="submit"
+                            className="flex gap-4 items-center justify-center mt-5 py-2.5 px-3 rounded-md shadow-md text-white bg-blue-400 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 focus:ring-offset-white"
+                        >
+                            Next
+                        </button>
+                    </form>
+                </section>
+            </section>
+        </section>
     );
 }
 
