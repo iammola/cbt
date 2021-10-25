6ca81c686dd11d0629056f66cbe7ee641a9b385b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 17:56:05 +0100

    initial commit of exam API route

---
+++ pages/api/exam.ts
@@ -0,0 +1,51 @@
+import { connect } from "db";
+import { AnswerModel } from "db/models/Answer";
+import { ExamModel, ExamRecord } from "db/models/Exam";
+import { QuestionModel, QuestionRecord } from "db/models/Question";
+import { startSession } from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
+import { RawQuestion } from "pages/create/questions";
+
+type RouteResponse = [boolean, number, string | Record<string, any> & { error?: unknown, message: string }];
+
+async function createExam({ exam, questions }: { exam: ExamRecord; questions: RawQuestion[] }): Promise<RouteResponse> {
+    await connect();
+    const session = await startSession();
+    let [success, status, message]: RouteResponse = [false, 501, ""];
+
+    try {
+        session.startTransaction();
+
+        const data = await ExamModel.create([{
+            ...exam,
+            questions: (await QuestionModel.create(await Promise.all(questions.map(async ({ answers, ...question }) => ({
+                ...question,
+                answers: (await AnswerModel.create(answers, { session })).map(({ _id }) => _id)
+            }))), { session })).map(({ _id }) => _id)
+        }], { session });
+
+        [success, status, message] = [true, 201, { data, message: "Success" }]
+        await session.commitTransaction();
+    } catch (error) {
+        [status, message] = [400, { error, message: "Couldn't CREATE Exam" }];
+        await session.abortTransaction();
+    }
+
+    session.endSession();
+
+    return [success, status, message];
+}
+
+export default async function handler({ body, method }: NextApiRequest, res: NextApiResponse) {
+    let [success, status, message]: RouteResponse = [false, 400, ""];
+    const allowedMethods = ["POST", "GET"];
+
+    if (allowedMethods.includes(method ?? '') === false) {
+        res.setHeader("Allow", allowedMethods);
+        [status, message] = [405, `Method ${method ?? ''} Not Allowed`];
+    } else [success, status, message] = method === "POST" ? await createExam(JSON.parse(body)) : [false, status, message];
+
+    if (typeof message !== "object") message = { message };
+
+    res.status(status).json({ success, ...message });
+}
