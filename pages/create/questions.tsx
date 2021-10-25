b84a43328f954cd4e637a3b56b9b9409d5ba4230
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 18:18:02 +0100

    initial commit of questions registration

--- pages/create/questions.tsx
+++ pages/create/questions.tsx
@@ -1,2 +1,12 @@
+import { NextPage } from "next";
 
 
+
+const CreateQuestions: NextPage = () => {
+    return (
+        <section className="flex flex-col items-center justify-center gap-2 w-screen">
+        </section>
+    );
+}
+
+export default CreateQuestions;
