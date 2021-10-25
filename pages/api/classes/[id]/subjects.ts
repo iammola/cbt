18b77a3b5337c845376be07f90f4ab5c25e02e7e
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 20:48:47 +0100

    added POST function to create subjects

--- pages/api/classes/[id]/subjects.ts
+++ pages/api/classes/[id]/subjects.ts
@@ -1,6 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { connect } from "db";
+import { ClassModel } from "db/models/Class";
+import { SubjectModel } from "db/models/Subject";
+
 type RouteResponse = [boolean, number, string | Record<string, any> & { error?: unknown, message: string }];
+
+async function createSubject(id: string, subjectData: typeof SubjectModel.schema.obj): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, 501, ""];
+
+    try {
+        const data = await SubjectModel.create(subjectData);
+        [success, status, message] = [true, 201, { data, message: "Created" }];
+    } catch (error) {
+        [status, message] = [400, { error, message: "Couldn't CREATE subject" }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method, query, body }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, 400, ""];
