b9ef1ac9a4a8d442a844b209c8d08ebcec0d7187
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 18:20:01 +0100

    added loading icon

--- components/CustomIcons.tsx
+++ components/CustomIcons.tsx
@@ -26,3 +26,28 @@ const LineIcon: FunctionComponent<ComponentProps<'svg'>> = (props) => {
 }
 
 export { CircleIcon, LineIcon };
+const LoadingIcon: FunctionComponent<ComponentProps<'svg'>> = (props) => {
+    return (
+        <svg
+            {...props}
+            fill="none"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <circle
+                r="10"
+                cx="12"
+                cy="12"
+                strokeWidth="4"
+                stroke="currentColor"
+                className="opacity-25"
+            />
+            <path
+                fill="currentColor"
+                className="opacity-75"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            />
+        </svg>
+    )
+}
+
