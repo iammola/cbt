69013bc07e13b3f87fe70d5ec717dd95728c0bd4
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 18:27:29 +0100

    added class create function

--- pages/api/classes/index.ts
+++ pages/api/classes/index.ts
@@ -1,6 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { connect } from "db";
+import { ClassModel } from "db/models/Class";
+
 type RouteResponse = [boolean, number, string | Record<string, any> & { error?: unknown, message: string }];
+
+async function createClass(item: typeof ClassModel.schema.obj): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, 501, ""];
+
+    try {
+        const data = await ClassModel.create(item);
+        [success, status, message] = [true, 201, { data, message: "Created" }];
+    } catch (error) {
+        [status, message] = [400, { error, message: "Couldn't CREATE Class" }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method, body }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, 400, ""];
