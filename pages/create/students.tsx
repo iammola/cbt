d9984c97888420822caf3ed7de73ecbbf4a6f9c8
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 21:07:36 +0100

    added title and description to page head

--- pages/create/students.tsx
+++ pages/create/students.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import { NextPage } from "next";
 
 const CreateStudents: NextPage = () => {
     return (
-        <></>
+        <>
+            <Head>
+                <title>Create Student Profile | CBT | Grand Regal School</title>
+                <meta name="description" content="Student Registration | GRS CBT" />
+            </Head>
+        </>
     );
 }
 
