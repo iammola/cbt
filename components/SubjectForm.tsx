def99d03c91c5c56dd9d690077d41f83e62ceb51
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 20:28:13 +0100

    added input fields and states

--- components/SubjectForm.tsx
+++ components/SubjectForm.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
+import { useState } from "react";
 
 const SubjectForm: NextPage = () => {
+    const [name, setName] = useState('');
+    const [alias, setAlias] = useState('');
     return (
         <>
             <form
@@ -10,6 +13,38 @@ const SubjectForm: NextPage = () => {
                     <span>Create a</span>{' '}
                     <span className="text-blue-500">Subject</span>
                 </h1>
+                <div className="flex flex-col gap-2.5 w-80">
+                    <label
+                        htmlFor="name"
+                        className="text-sm text-gray-600 font-semibold"
+                    >
+                        Name
+                    </label>
+                    <input
+                        required
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={({ target: { value } }) => setName(value)}
+                        className="border rounded-md transition-shadow focus:ring-2 focus:ring-blue-400 focus:outline-none p-3 pl-5"
+                    />
+                </div>
+                <div className="flex flex-col gap-2.5 w-80">
+                    <label
+                        htmlFor="alias"
+                        className="text-sm text-gray-600 font-semibold"
+                    >
+                        Alias
+                    </label>
+                    <input
+                        required
+                        id="alias"
+                        type="text"
+                        value={alias}
+                        onChange={({ target: { value } }) => setAlias(value)}
+                        className="border rounded-md transition-shadow focus:ring-2 focus:ring-blue-400 focus:outline-none p-3 pl-5"
+                    />
+                </div>
                 <button
                     type="submit"
                 >
