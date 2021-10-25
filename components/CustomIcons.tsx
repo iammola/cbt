79284f318dbb4daf4939806cd5fd73ec24f5a5f0
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 18:19:42 +0100

    added OuterCircle stroke icon

--- components/CustomIcons.tsx
+++ components/CustomIcons.tsx
@@ -8,6 +8,15 @@ const CircleIcon: FunctionComponent<ComponentProps<'svg'>> = (props) => {
     )
 }
 
+const OuterCircleIcon: FunctionComponent<ComponentProps<'svg'>> = (props) => {
+    return (
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <circle cx="8" cy="8" r="8" fill="#FFF" stroke="currentColor" strokeLinejoin="round" />
+            <circle cx="8" cy="8" r="5" fill="currentColor" />
+        </svg>
+    )
+}
+
 const LineIcon: FunctionComponent<ComponentProps<'svg'>> = (props) => {
     return (
         <svg width="2" viewBox="0 0 2 10" fill="none" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" {...props}>
