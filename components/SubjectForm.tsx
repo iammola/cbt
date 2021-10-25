4d9dc316b9ca8b4ee2eebda6ceb9da9ee88def87
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 20:34:54 +0100

    added onSubmit event to form

--- components/SubjectForm.tsx
+++ components/SubjectForm.tsx
@@ -58,6 +58,7 @@ const SubjectForm: NextPage = () => {
     return (
         <>
             <form
+                onSubmit={handleSubmit}
                 className="flex flex-col gap-7 rounded-3xl shadow-lg p-8 bg-white"
             >
                 <h1 className="text-4xl text-gray-800 font-bold tracking-tight text-center pb-4">
