762e1b62de3937ad5f8909e5af9d397d7954900d
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 20:40:12 +0100

    initial commit of class subjects API route

---
+++ pages/api/classes/[id]/subjects.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type RouteResponse = [boolean, number, string | Record<string, any> & { error?: unknown, message: string }];
+
+export default async function handler({ method, query, body }: NextApiRequest, res: NextApiResponse) {
+    let [success, status, message]: RouteResponse = [false, 400, ""];
+    const allowedMethods = ["POST", "GET"];
+
+    if (allowedMethods.includes(method ?? '') === false) {
+        res.setHeader("Allow", allowedMethods);
+        [status, message] = [405, `Method ${method ?? ''} Not Allowed`];
+    }
+
+    if (typeof message !== "object") message = { message };
+
+    res.status(status).json({ success, ...message });
+}
