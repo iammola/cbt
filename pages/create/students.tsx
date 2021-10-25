9cf8407f7033a0f4333e5d50c61df6c93203b7e3
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 21:31:42 +0100

    added required input fields

--- pages/create/students.tsx
+++ pages/create/students.tsx
@@ -1,13 +1,148 @@
 import Head from "next/head";
 import { NextPage } from "next";
+import { useState } from "react";
+
+import Select from "components/Select";
 
 const CreateStudents: NextPage = () => {
+    const [selectedClass, setSelectedClass] = useState({
+        _id: "",
+        name: "Select class"
+    });
+
     return (
         <>
             <Head>
                 <title>Create Student Profile | CBT | Grand Regal School</title>
                 <meta name="description" content="Student Registration | GRS CBT" />
             </Head>
+            <section className="flex flex-col md:flex-row items-center justify-center gap-y-20 md:gap-y-0 gap-x-0 md:gap-x-10 p-10 w-screen min-h-screen">
+                <form className="flex flex-col gap-7 rounded-3xl shadow-lg p-8 bg-white">
+                    <h1 className="text-4xl text-gray-800 font-bold tracking-tight text-center pb-4">
+                        <span>Create a</span>{' '}
+                        <span className="text-purple-500">Student Profile</span>
+                    </h1>
+                    <div className="flex flex-col gap-2.5 min-w-80 w-full">
+                        <label
+                            htmlFor="fullName"
+                            className="text-sm text-gray-600 font-semibold"
+                        >
+                            Full Name
+                        </label>
+                        <input
+                            required
+                            type="text"
+                            id="fullName"
+                            className="border rounded-md transition-shadow focus:ring-2 focus:ring-purple-400 focus:outline-none p-3 pl-5"
+                        />
+                    </div>
+                    <div className="flex items-center justify-between gap-4 w-full">
+                        <div className="flex flex-col gap-2.5">
+                            <label
+                                htmlFor="initials"
+                                className="text-sm text-gray-600 font-semibold"
+                            >
+                                Initials
+                            </label>
+                            <input
+                                required
+                                type="text"
+                                id="initials"
+                                className="border rounded-md transition-shadow focus:ring-2 focus:ring-purple-400 focus:outline-none p-3 pl-5"
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2.5">
+                            <label
+                                htmlFor="email"
+                                className="text-sm text-gray-600 font-semibold"
+                            >
+                                E-mail
+                            </label>
+                            <input
+                                required
+                                id="email"
+                                type="email"
+                                className="border rounded-md transition-shadow focus:ring-2 focus:ring-purple-400 focus:outline-none p-3 pl-5"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-between gap-4 w-full">
+                        <div className="flex flex-col gap-2.5">
+                            <label
+                                htmlFor="firstName"
+                                className="text-sm text-gray-600 font-semibold"
+                            >
+                                First Name
+                            </label>
+                            <input
+                                required
+                                type="text"
+                                id="firstName"
+                                className="border rounded-md transition-shadow focus:ring-2 focus:ring-purple-400 focus:outline-none p-3 pl-5"
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2.5">
+                            <label
+                                htmlFor="lastName"
+                                className="text-sm text-gray-600 font-semibold"
+                            >
+                                Last Name
+                            </label>
+                            <input
+                                required
+                                type="text"
+                                id="lastName"
+                                className="border rounded-md transition-shadow focus:ring-2 focus:ring-purple-400 focus:outline-none p-3 pl-5"
+                            />
+                        </div>
+                    </div>
+                    <Select
+                        label="Classes"
+                        colorPallette={{
+                            activeCheckIconColor: "text-purple-600",
+                            inactiveCheckIconColor: "text-purple-800",
+                            activeOptionColor: "text-purple-900 bg-purple-100",
+                            buttonBorderColor: "focus-visible:border-purple-500",
+                            buttonOffsetFocusColor: "focus-visible:ring-offset-purple-500"
+                        }}
+                        options={[]}
+                        selected={selectedClass}
+                        handleChange={setSelectedClass}
+                    />
+                    <div className="flex flex-col gap-2 5 min-w-80 w-full">
+                        <span className="text-sm text-gray-600 font-semibold">
+                            Subjects
+                        </span>
+                        <div className="flex gap-x-4 gap-y-3 w-full text-sm">
+                            {[{
+                                _id: "",
+                                name: "Mathematics"
+                            }, {
+                                _id: "",
+                                name: "English Language"
+                            }].map(({ _id, name }) => (
+                                <label
+                                    key={_id}
+                                    htmlFor={_id}
+                                    className="flex gap-3 p-2"
+                                >
+                                    <input
+                                        id={_id}
+                                        type="checkbox"
+                                    />
+                                    {name}
+                                </label>
+                            ))}
+                        </div>
+                    </div>
+                    <button
+                        type="submit"
+                        className="flex gap-4 items-center justify-center mt-3 py-2.5 px-3 rounded-md shadow-md text-white transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-offset-white bg-purple-400 hover:bg-purple-500 focus:ring-purple-500"
+                    >
+                        Create Profile
+                    </button>
+                </form>
+            </section>
         </>
     );
 }
