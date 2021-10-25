f9804988617a91b13fad1c7d6019e6fa371168ff
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 18:25:51 +0100

    initial commit of class and subject form

---
+++ pages/create/class.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { NextPage } from "next";
+import { useState } from "react";
+
+import ClassForm from "components/ClassForm";
+import SubjectForm from "components/SubjectForm";
+
+const CreateClass: NextPage = () => {
+    const [name, setName] = useState('');
+    const [selectedClass, setSelectedClass] = useState('');
+
+    return (
+        <>
+            <Head>
+                <title>Create a Class | CBT | Grand Regal School</title>
+                <meta name="description" content="Class Registration Page | GRS CBT" />
+            </Head>
+            <section className="flex flex-col md:flex-row items-center justify-center gap-y-20 md:gap-y-0 gap-x-0 md:gap-x-10 p-10 w-screen min-h-screen">
+                <ClassForm />
+                <SubjectForm />
+            </section>
+        </>
+    );
+}
+
+export default CreateClass;
