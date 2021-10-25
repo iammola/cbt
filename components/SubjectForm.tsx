03e0360551f4089e05bcdfa62cadc2941bb9103f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 20:26:38 +0100

    added form and submit button

--- components/SubjectForm.tsx
+++ components/SubjectForm.tsx
@@ -3,6 +3,19 @@ import { NextPage } from "next";
 const SubjectForm: NextPage = () => {
     return (
         <>
+            <form
+                className="flex flex-col gap-7 rounded-3xl shadow-lg p-8 bg-white"
+            >
+                <h1 className="text-4xl text-gray-800 font-bold tracking-tight text-center pb-4">
+                    <span>Create a</span>{' '}
+                    <span className="text-blue-500">Subject</span>
+                </h1>
+                <button
+                    type="submit"
+                >
+                    Create Subject
+                </button>
+            </form>
         </>
     );
 }
