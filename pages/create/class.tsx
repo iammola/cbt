164ce078cb95617518116b6b2c604f32acd9e9f2
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-25 21:05:32 +0100

    changed page title and description

--- pages/create/class.tsx
+++ pages/create/class.tsx
@@ -8,8 +8,8 @@ const CreateClass: NextPage = () => {
     return (
         <>
             <Head>
-                <title>Create a Class | CBT | Grand Regal School</title>
-                <meta name="description" content="Class Registration Page | GRS CBT" />
+                <title>Create a Class // Subject | CBT | Grand Regal School</title>
+                <meta name="description" content="Class // Subject Registration | GRS CBT" />
             </Head>
             <section className="flex flex-col md:flex-row items-center justify-center gap-y-20 md:gap-y-0 gap-x-0 md:gap-x-10 p-10 w-screen min-h-screen">
                 <ClassForm />
