7448bd2556c59b607412967a77c525e181083e46
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-26 09:29:56 +0100

    added POST function to create student

--- pages/api/students/index.ts
+++ pages/api/students/index.ts
@@ -1,6 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { connect } from "db";
+import { SessionModel } from "db/models/Session";
+import { StudentModel, StudentRecord } from "db/models/Student";
+
 type RouteResponse = [boolean, number, string | Record<string, any> & { error?: unknown, message: string }];
+
+async function createStudent({ academic, ...student }: Pick<StudentRecord, 'email' | 'name'> & { academic: { class: string; subject: string; } }): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, 501, ""];
+
+    try {
+        const currentSession = await SessionModel.findOne({ current: true, "terms.current": { $elemMatch: { current: true } } }).select('terms')
+
+        const data = await StudentModel.create({
+            ...student,
+            academic: currentSession === null ? [] : [{
+                session: currentSession._id,
+                terms: [{
+                    ...academic,
+                    term: currentSession.terms[0]._id
+                }]
+            }]
+        });
+        [success, status, message] = [true, 201, { data, message: "Created" }];
+    } catch (error) {
+        [status, message] = [400, { error, message: "Couldn't CREATE Class" }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method, query, body }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, 400, ""];
