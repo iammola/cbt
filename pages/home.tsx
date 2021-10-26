8c495e0d6c01cfcc364290f5783769538ac28a72
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-27 00:02:40 +0100

    added main body element

--- pages/home.tsx
+++ pages/home.tsx
@@ -6,6 +6,8 @@ const Home: NextPage = (props) => {
     return (
         <section className="flex flex-col items-center justify-start gap-2 w-screen divide-y-[1.5px] divide-gray-200">
             <Navbar />
+            <main className="flex divide-x-[1.5px] divide-gray-200 w-full">
+            </main>
         </section>
     )
 }
