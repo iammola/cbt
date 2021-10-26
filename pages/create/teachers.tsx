7aba8f35e5e4cf0941dce323886c8d1894d55622
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-26 12:09:48 +0100

    initial commit of create teachers page

---
+++ pages/create/teachers.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+const CreateTeachers: NextPage = () => {
+
+    return (
+        <>
+            <Head>
+                <title>Create Teacher Profile | CBT | Grand Regal School</title>
+                <meta name="description" content="Teacher Registration | GRS CBT" />
+            </Head>
+            <section className="flex flex-col md:flex-row items-center justify-center gap-y-20 md:gap-y-0 gap-x-0 md:gap-x-10 p-10 w-screen min-h-screen">
+            </section>
+        </>
+    );
+}
+
+export default CreateTeachers;
