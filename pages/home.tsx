0343fd99dafdf4dd585aeb252e9cedecccc01e9a
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-26 23:36:40 +0100

    imported Navbar component

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from "next";
 
+import { Navbar } from "components/Layout";
+
 const Home: NextPage = (props) => {
     return (
-        <>
-        </>
+        <section className="flex flex-col items-center justify-start gap-2 w-screen">
+            <Navbar />
+        </section>
     )
 }
 
