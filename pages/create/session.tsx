9bcd71ad34b7856d5ff70daf83b0e642e00ba44c
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-26 03:15:03 +0100

    initial commit of create session page

---
+++ pages/create/session.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+const CreateSession: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Create a School Session // Term | CBT | Grand Regal School</title>
+                <meta name="description" content="School Session // Term Registration | GRS CBT" />
+            </Head>
+        </>
+    )
+}
