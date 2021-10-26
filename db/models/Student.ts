a0cdf727e28d3bf0a67c922141929619c9676c96
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-26 11:41:27 +0100

    set _id to false to prevent automatic _ids

--- db/models/Student.ts
+++ db/models/Student.ts
@@ -38,7 +38,8 @@ const StudentSchema = new Schema<StudentRecord>({
                 trim: true,
                 required: [true, 'Last Name required']
             }
-        }
+        },
+        _id: false,
     }, email: {
         type: String,
         trim: true,
@@ -46,6 +47,7 @@ const StudentSchema = new Schema<StudentRecord>({
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/, 'Please fill a valid email address']
     }, academic: [{
         type: {
+            _id: false,
             session: {
                 type: Schema.Types.ObjectId,
                 required: [true, 'Session required']
@@ -58,7 +60,8 @@ const StudentSchema = new Schema<StudentRecord>({
                     }, subjects: [{
                         type: Schema.Types.ObjectId
                     }]
-                }, validate: [(v: StudentRecord['academic']) => v.every(i => i.terms.length > 0), 'At least one term required']
+                }, validate: [(v: StudentRecord['academic']) => v.every(i => i.terms.length > 0), 'At least one term required'],
+                _id: false,
             }]
         },
     }]
