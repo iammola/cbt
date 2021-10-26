6da33e0c1e098cedffc90aab07d40c5a9123997c
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-27 00:02:04 +0100

    modified styles to add border divide

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "components/Layout";
 
 const Home: NextPage = (props) => {
     return (
-        <section className="flex flex-col items-center justify-start gap-2 w-screen">
+        <section className="flex flex-col items-center justify-start gap-2 w-screen divide-y-[1.5px] divide-gray-200">
             <Navbar />
         </section>
     )
