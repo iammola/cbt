828b4d05cdd23adee25de722a7d9c0e619a5865e
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-27 16:46:24 +0100

    added page title and description

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,15 +1,22 @@
+import Head from "next/head";
 import { NextPage } from "next";
 
 import { Navbar, Sidebar } from "components/Layout";
 
 const Home: NextPage = (props) => {
     return (
-        <section className="flex flex-col items-center justify-start gap-2 w-screen divide-y-[1.5px] divide-gray-200">
-            <Navbar />
-            <main className="flex divide-x-[1.5px] divide-gray-200 w-full h-full">
-                <Sidebar />
-            </main>
-        </section>
+        <>
+            <Head>
+                <title>Home | CBT | Grand Regal School</title>
+                <meta name="description" content="Home | GRS CBT" />
+            </Head>
+            <section className="flex flex-col items-center justify-start gap-2 w-screen divide-y-[1.5px] divide-gray-200">
+                <Navbar />
+                <main className="flex divide-x-[1.5px] divide-gray-200 w-full h-full">
+                    <Sidebar />
+                </main>
+            </section>
+        </>
     )
 }
 
