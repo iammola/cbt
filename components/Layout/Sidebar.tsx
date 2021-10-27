63b4b11f55379285efaf7ac73c1faf4ba27ab2b9
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-27 13:50:33 +0100

    imported Calendar component

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,9 +1,14 @@
 import { FunctionComponent } from "react";
 
+import { Calendar } from "components/Misc";
+
 const Sidebar: FunctionComponent = () => {
     return (
         <aside>
-
+            <Calendar
+                month={9}
+                year={2021}
+            />
         </aside>
     )
 }
