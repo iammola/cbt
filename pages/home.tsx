cd9a18396fd641e943ec0ffd842c38150a8d8ca2
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-27 13:53:02 +0100

    imported Sidebar

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,12 +1,13 @@
 import { NextPage } from "next";
 
-import { Navbar } from "components/Layout";
+import { Navbar, Sidebar } from "components/Layout";
 
 const Home: NextPage = (props) => {
     return (
         <section className="flex flex-col items-center justify-start gap-2 w-screen divide-y-[1.5px] divide-gray-200">
             <Navbar />
             <main className="flex divide-x-[1.5px] divide-gray-200 w-full">
+                <Sidebar />
             </main>
         </section>
     )
