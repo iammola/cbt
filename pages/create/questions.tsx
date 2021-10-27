12ab014c53dcacba91b9f7155fc2903b03d1691f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-27 21:57:38 +0100

    used selected exam

--- pages/create/questions.tsx
+++ pages/create/questions.tsx
@@ -54,9 +54,9 @@ const CreateQuestions: NextPage = () => {
             <div className="flex items-center justify-start gap-2 text-gray-400 pt-7 pb-2 px-10 w-full text-sm font-medium">
                 <span>2021/2022 Third Term</span>
                 <ChevronRightIcon className="w-5 h-5 text-gray-500" />
-                <span>Key Stage 1</span>
+                <span>{exam?.class ?? "Select Class"}</span>
                 <ChevronRightIcon className="w-5 h-5 text-gray-500" />
-                <span>Civic Education</span>
+                <span>{exam?.subject ?? "Select Subject"}</span>
                 <ChevronRightIcon className="w-5 h-5 text-gray-500" />
                 <span className="text-gray-600">Questions</span>
             </div>
