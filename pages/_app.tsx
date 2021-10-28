1d70dacd65046b2a37ac8a2aeb5e6e830e1d8c20
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-28 14:59:57 +0100

    removed getInitialProps
    after update to Next.JS 12, I prefer the use of the _middleware.ts file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import Head from "next/head";
-import { parse } from "cookie";
-import { CookiesProvider } from "react-cookie";
-import App, { AppContext, AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
 
 import 'styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <CookiesProvider>
+        <>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -17,17 +15,8 @@ function MyApp({ Component, pageProps }: AppProps) {
             <main id="main">
                 <Component {...pageProps} />
             </main>
-        </CookiesProvider>
+        </>
     )
-}
-
-MyApp.getInitialProps = async (appContext: AppContext) => {
-    const { ctx: { req, res, pathname } } = appContext
-    const cookies = parse(req?.headers.cookie ?? '');
-
-    if (cookies.account === undefined && pathname !== "/") res?.writeHead(401, { Location: `/?return=${pathname}` }).end();
-
-    return { ...(await App.getInitialProps(appContext)) }
 }
 
 export default MyApp;
