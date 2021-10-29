73993bf42a48183afb59e93a6dac5edb96e9fcd9
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 02:52:47 +0100

    added GET method implementation

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -1,6 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { connect } from "db";
+import { EventModel } from "db/models";
+
 import { RouteResponse } from "types";
+
+async function getEvents({ from, to, exact }: { from: string; to: string; exact?: string; }): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, 400, ""];
+
+    try {
+        const data = await EventModel.find({ date: exact !== undefined ? new Date(exact) : { $gte: new Date(+from), $lte: new Date(+to) } }).lean();
+        [success, status, message] = [true, 200, { data, message: "Success" }];
+    } catch (error) {
+        [status, message] = [400, { error, message: "Couldn't GET events" }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ body, query, method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, 400, ""];
