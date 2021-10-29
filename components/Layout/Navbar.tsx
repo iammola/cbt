1fb8f9d70a0a10ac4545da8b56fead67d8e28cd5
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 09:19:59 +0100

    modified styles to change color

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -84,8 +84,8 @@ const Navbar: FunctionComponent = () => {
                 </ul>
             </nav>
             {account !== undefined && (
-                    <div className="flex items-center justify-center relative rounded-full bg-blue-500 flex-shrink-0 w-10 h-10">
                 <div className="flex flex-shrink-0 items-center gap-5 my-4 p-3 cursor-pointer rounded-lg transition-colors hover:bg-gray-100">
+                    <div className="flex items-center justify-center relative rounded-full bg-indigo-500 flex-shrink-0 w-10 h-10">
                         <UserImage
                             image=""
                             className="rounded-full"
