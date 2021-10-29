cfd475a25cb9b68af6110c8cfde07eb15506036f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 02:44:01 +0100

    grouped models

---
+++ db/models/index.ts
@@ -0,0 +1,13 @@
+export { ExamModel } from "./Exam";
+export { EventModel } from "./Event";
+
+export { SessionModel } from "./Session";
+
+export { ClassModel } from "./Class";
+export { SubjectModel } from "./Subject";
+
+export { StudentModel } from "./Student";
+export { TeacherModel } from "./Teacher";
+
+export { AnswerModel } from "./Answer";
+export { QuestionModel } from "./Question";
