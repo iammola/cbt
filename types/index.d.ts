832059fac68bb9ce03477b8494a2bd83fc77140b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 02:43:25 +0100

    initial commit of global types

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+export * from "./models";
+
+import { QuestionRecord } from "./models";
+
+export type CreateQuestion = Omit<QuestionRecord, 'answers'> & {
+    answers: Omit<NonNullable<QuestionRecord<true>['answers']>[number], '_id'>[];
+};
+
+export type RouteResponse = [boolean, number, string | Record<string, any> & { error?: unknown, message: string }];
