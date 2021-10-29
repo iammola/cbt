3b6f3a04a0a3678825f93545cd76657b7d0d1b3d
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 21:51:42 +0100

    implemented GET method function

--- pages/api/sessions/current.ts
+++ pages/api/sessions/current.ts
@@ -1,7 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { SessionModel } from "db/models";
+
 import { RouteResponse } from "types";
+
+async function getCurrentSession(): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const data = await SessionModel.findOne({ current: true }, { name: 1, alias: 1, terms: { $elemMatch: { current: true } } });
+        [success, status, message] = [true, StatusCodes.OK, {
+            data,
+            message: ReasonPhrases.OK
+        }];
+    } catch (error) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method, query, cookies, body }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
