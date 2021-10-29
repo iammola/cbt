09366d4357dc453fc6bda40681054ff997616cdc
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 02:01:18 +0100

    initial commit of Event schema

---
+++ db/models/Event.ts
@@ -0,0 +1,31 @@
+import { isFuture } from "date-fns";
+import { Schema, Model, model, models } from "mongoose";
+
+export type EventRecord<P = false> = {
+    date: Date;
+    events: {
+        name: string;
+        subject: Schema.Types.ObjectId;
+    }[];
+} & (P extends true ? { _id: string } : {})
+
+const EventSchema = new Schema<EventRecord>({
+    date: {
+        type: Date,
+        required: [true, 'Event date is required'],
+        validate: [(v: Date) => isFuture(v), 'New dates must be after the current date']
+    }, events: [{
+        type: {
+            _id: false,
+            name: {
+                type: String,
+                required: [true, 'Event requires a name']
+            }, subject: {
+                type: Schema.Types.ObjectId,
+                required: [true, 'Event subject meh']
+            }
+        }
+    }]
+});
+
+export const EventModel = models.Event as Model<EventRecord> ?? model('Event', EventSchema);
