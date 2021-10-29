f3d07b36fb15268a8f7a42af6ea9a7cba7eeb15d
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 09:13:17 +0100

    modified styles to remove gap

--- pages/home.tsx
+++ pages/home.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
                 <title>Home | CBT | Grand Regal School</title>
                 <meta name="description" content="Home | GRS CBT" />
             </Head>
-            <section className="flex flex-col items-center justify-start gap-2 w-screen divide-y-[1.5px] divide-gray-200">
+            <section className="flex flex-col items-center justify-start w-screen divide-y-[1.5px] divide-gray-200">
                 <Navbar />
                 <main className="flex divide-x-[1.5px] divide-gray-200 w-full h-full">
                     <section className="flex-grow w-full h-full"></section>
