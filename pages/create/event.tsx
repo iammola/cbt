6ecb62dbd861404e84267e0f90aefe3286747605
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 12:40:04 +0100

    initial commit of create event page

---
+++ pages/create/event.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+const CreateEvent: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Create Event | CBT | Grand Regal School</title>
+                <meta name="description" content="Event Registration | GRS CBT" />
+            </Head>
+        </>
+    );
+}
+
+export default CreateEvent;
