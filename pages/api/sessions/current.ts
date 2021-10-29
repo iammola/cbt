167cc3e596b894563486597b6d8b4e755c6166de
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-29 21:50:49 +0100

    initial commit of current session API route

---
+++ pages/api/sessions/current.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+import { RouteResponse } from "types";
+
+export default async function handler({ method, query, cookies, body }: NextApiRequest, res: NextApiResponse) {
+    let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+    const allowedMethods = "GET";
+
+    if (allowedMethods !== method) {
+        res.setHeader("Allow", allowedMethods);
+        [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
+    }
+
+    if (typeof message !== "object") message = { message };
+
+    res.status(status).json({ success, ...message });
+}
