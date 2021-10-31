2df4ed643b0d0097df456078389ffc1270d46686
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-31 18:42:06 +0100

    added children

--- components/Misc/Notification.tsx
+++ components/Misc/Notification.tsx
@@ -14,7 +14,20 @@ const Notification: FunctionComponent<NotificationProps> = ({ timeout, message, 
             leave="ease-in duration-200 transition-transform"
             leaveFrom="translate-x-0"
             leaveTo="translate-x-full"
-        ></Transition>
+        >
+            <div className="flex items-center justify-start gap-6 w-96 min-h-[4rem] p-4 pl-6 border border-gray-200 bg-white rounded-lg shadow-sm">
+                <Icon />
+                <p className="flex-grow text-sm text-gray-900 block truncate">
+                    {message}
+                </p>
+                <button
+                    onClick={closeNotification}
+                    className="p-1 rounded-md hover:bg-gray-200 pointer-events-auto"
+                >
+                    <XIcon className="w-5 h-5 text-gray-500" />
+                </button>
+            </div>
+        </Transition>
     );
 }
 
