fbda617cc8e1ed174ca784a4ce093cd9fedc9a51
Ademola Adedeji <a.mola.dev@gmail.com>
2021-10-31 12:40:04 +0100

    added Transition wrapping element

--- components/Misc/Notification.tsx
+++ components/Misc/Notification.tsx
@@ -1,8 +1,20 @@
-import { ComponentProps, FunctionComponent } from "react";
+import { Transition } from "@headlessui/react";
+import { ComponentProps, Fragment, FunctionComponent } from "react";
+
 const Notification: FunctionComponent<NotificationProps> = ({ timeout, message, Icon }) => {
 
     return (
-        <></>
+        <Transition
+            appear
+            show
+            as={Fragment}
+            enter="ease-out duration-300 transition-transform"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="ease-in duration-200 transition-transform"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+        ></Transition>
     );
 }
 
