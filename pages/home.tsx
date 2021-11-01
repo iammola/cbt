86a9411c53312cba4417c6db4d8648ba11cc4d3f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 16:46:22 +0100

    corrected Sidebar import and export

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { NextPage } from "next";
 
-import { Navbar, Sidebar } from "components/Layout";
+import { Navbar, ScheduleInfo } from "components/Layout";
 
 const Home: NextPage = () => {
     return (
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
                 <Navbar />
                 <main className="flex flex-col md:flex-row divide-x-[1.5px] divide-gray-200 w-full h-full">
                     <section className="flex-grow w-full h-full"></section>
-                    <Sidebar />
+                    <ScheduleInfo />
                 </main>
             </section>
         </>
