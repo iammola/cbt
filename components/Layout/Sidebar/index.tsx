17c8e91c5968f85315f78dc407c8ac55a71a3293
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 17:31:24 +0100

    modified parent styles

--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -6,8 +6,8 @@ const Sidebar: FunctionComponent = () => {
     const [open, setOpen] = useState(false);
 
     return (
-        <aside className={classNames("h-screen bg-white rounded-lg shadow-md overflow-hidden relative", {
-            "w-screen sm:w-96": open,
+        <aside className={classNames("flex flex-col gap-5 items-center justify-start py-5 px-3 max-w-full h-screen bg-white rounded-lg shadow-md overflow-hidden relative", {
+            "w-screen sm:w-[20rem]": open,
             "w-14 sm:w-[4.5rem]": open === false
         })}>
 
