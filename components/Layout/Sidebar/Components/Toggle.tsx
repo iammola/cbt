9b91582e70fcb5df90cb13a181beec10b036b74e
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 22:01:53 +0100

    added ToggleProps

--- components/Layout/Sidebar/Components/Toggle.tsx
+++ components/Layout/Sidebar/Components/Toggle.tsx
@@ -1,9 +1,14 @@
 import { FunctionComponent } from "react";
 
-const Toggle: FunctionComponent = () => {
+const Toggle: FunctionComponent<ToggleProps> = ({ open, toggleOpen }) => {
     return (
         <></>
     );
+}
+
+type ToggleProps = {
+    open: boolean;
+    toggleOpen(): void;
 }
 
 export default Toggle;
---
+++ components/Layout/Sidebar/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Menu } from "./Menu";
+export { default as Brand } from "./Brand";
+export { default as Toggle } from "./Toggle";
