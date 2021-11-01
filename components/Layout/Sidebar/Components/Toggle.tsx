e8e0aa8e94d1a7d30c7f7435df4a4faedd949c1a
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 22:02:23 +0100

    added elements

--- components/Layout/Sidebar/Components/Toggle.tsx
+++ components/Layout/Sidebar/Components/Toggle.tsx
@@ -1,8 +1,26 @@
 import { FunctionComponent } from "react";
+import { ChevronRightIcon, XIcon } from "@heroicons/react/solid";
+
+import { classNames } from "utils";
 
 const Toggle: FunctionComponent<ToggleProps> = ({ open, toggleOpen }) => {
     return (
-        <></>
+        <div
+            onClick={toggleOpen}
+            className={classNames("bg-white absolute p-1 rounded-full shadow-md text-gray-600", {
+                "sm:-right-4 right-8": open,
+                "-right-12": open === false
+            })}
+        >
+            <ChevronRightIcon
+                className={classNames("w-5 h-5 transition-transform", {
+                    "rotate-180 hidden sm:block": open
+                })}
+            />
+            {open === true && (
+                <XIcon className="block sm:hidden w-5 h-5" />
+            )}
+        </div>
     );
 }
 
