db011317fad129bcd9c0cc47fc4d024f9a4ddfcf
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 00:43:51 +0100

    increased width

--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -8,7 +8,7 @@ const Sidebar: FunctionComponent = () => {
     return (
         <aside className={classNames("flex flex-col gap-5 items-center justify-start py-5 px-3 max-w-full h-screen bg-white rounded-lg shadow-sm relative", {
             "w-screen sm:w-[20rem]": open,
-            "w-14 sm:w-[4.5rem]": open === false
+            "w-20 sm:w-[6.5rem]": open === false
         })}>
 
         </aside>
