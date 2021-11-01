625e5fa53e20e74129baa68c2b79d0ca265545c9
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 18:15:59 +0100

    added base menu items

--- components/Layout/Sidebar/Components/Menu.tsx
+++ components/Layout/Sidebar/Components/Menu.tsx
@@ -1,8 +1,29 @@
 import { FunctionComponent } from "react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { DocumentReportIcon, HomeIcon } from "@heroicons/react/outline";
 
 const Menu: FunctionComponent = () => {
     return (
-        <></>
+        <nav className="flex-grow w-full px-5">
+            <span className="inline-block text-gray-600 text-xs font-semibold uppercase tracking-wider pb-4">
+                Menu
+            </span>
+            <ul className="flex flex-col gap-y-2 items-center justify-center w-full">
+                <li className="flex gap-2.5 items-center justify-start w-full rounded-lg text-gray-600 hover:text-gray-800 hover:bg-gray-100 py-2.5 pr-3 cursor-pointer">
+                    <HomeIcon className="w-6 h-6 ml-3" />
+                    <span className="text-sm">
+                        Dashboard
+                    </span>
+                </li>
+                <li className="flex gap-2.5 items-center justify-start w-full rounded-lg text-gray-600 hover:text-gray-800 hover:bg-gray-100 py-2.5 pr-3 cursor-pointer">
+                    <DocumentReportIcon className="w-6 h-6 ml-3" />
+                    <span className="text-sm">
+                        Exams
+                    </span>
+                    <ChevronDownIcon className="w-5 h-5 ml-auto text-gray-600" />
+                </li>
+            </ul>
+        </nav>
     );
 }
 
