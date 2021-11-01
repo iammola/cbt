d6db1d19a275e190a875f08d0a3617a7c516d7ff
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 14:58:41 +0100

    added Promise.any polyfill polyfill #1

--- utils/index.ts
+++ utils/index.ts
@@ -8,3 +8,19 @@ export function classNames(...arg: (string | string[] | { [key: string]: any } |
 export function generateCode() {
     return Math.floor(1e5 + Math.random() * 9e5);
 }
+
+/**
+ * @link https://dev.to/sinxwal/looking-for-promise-any-let-s-quickly-implement-a-polyfill-for-it-1kga
+ * @param iterable promise return value
+ * @returns The first Promise that resolves or an Error if they all reject
+ */
+export function promiseAnyPolyfill<T>(iterable: Iterable<T | PromiseLike<T>>): Promise<T> {
+    return Promise.all(
+        [...iterable].map(promise => new Promise((resolve, reject) =>
+            Promise.resolve(promise).then(reject, resolve)
+        ))
+    ).then(
+        errors => Promise.reject(errors),
+        value => Promise.resolve<T>(value)
+    );
+}
