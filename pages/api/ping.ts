5ae21111024ac47f0894555c93793bf3086f18bf
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 03:04:08 +0100

    added db connection

--- pages/api/ping.ts
+++ pages/api/ping.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+
 import { RouteResponse } from "types";
 
 export default async function handler({ body, method }: NextApiRequest, res: NextApiResponse) {
@@ -10,6 +12,22 @@ export default async function handler({ body, method }: NextApiRequest, res: Nex
     if (allowedMethods !== method) {
         res.setHeader("Allow", allowedMethods);
         [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
+    } else {
+        try {
+            const db = await connect();
+            [success, status, message] = [true, StatusCodes.OK, {
+                data: {
+                    code: db.connection.readyState,
+                    state: db.STATES[db.connection.readyState]
+                },
+                message: ReasonPhrases.OK
+            }];
+        } catch (error) {
+            [status, message] = [StatusCodes.INTERNAL_SERVER_ERROR, {
+                error,
+                message: ReasonPhrases.INTERNAL_SERVER_ERROR
+            }]
+        }
     }
 
     if (typeof message !== "object") message = { message };
