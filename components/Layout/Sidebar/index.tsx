2cfcf36517408aa171eaf5a8a83df003b43acbf3
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 22:02:56 +0100

    removed `overflow` styles

--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -6,7 +6,7 @@ const Sidebar: FunctionComponent = () => {
     const [open, setOpen] = useState(false);
 
     return (
-        <aside className={classNames("flex flex-col gap-5 items-center justify-start py-5 px-3 max-w-full h-screen bg-white rounded-lg shadow-md overflow-hidden relative", {
+        <aside className={classNames("flex flex-col gap-5 items-center justify-start py-5 px-3 max-w-full h-screen bg-white rounded-lg shadow-sm relative", {
             "w-screen sm:w-[20rem]": open,
             "w-14 sm:w-[4.5rem]": open === false
         })}>
