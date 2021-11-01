b36424a1386e69688243bbcc2e97e6b25ffd8d50
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 00:48:17 +0100

    imported components

--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -2,6 +2,10 @@ import { FunctionComponent, useState } from "react";
 
 import { classNames } from "utils";
 
+import { Brand, Menu, Toggle } from "./components";
+
+import { Divide } from "components/Misc";
+
 const Sidebar: FunctionComponent = () => {
     const [open, setOpen] = useState(false);
 
@@ -10,7 +14,14 @@ const Sidebar: FunctionComponent = () => {
             "w-screen sm:w-[20rem]": open,
             "w-20 sm:w-[6.5rem]": open === false
         })}>
-
+            <Toggle
+                open={open}
+                toggleOpen={() => setOpen(!open)}
+            />
+            <Brand open={open} />
+            <Divide className="w-[85%] max-w-full" />
+            <Menu open={open} />
+            <Divide className="w-[85%] max-w-full" />
         </aside>
     );
 }
