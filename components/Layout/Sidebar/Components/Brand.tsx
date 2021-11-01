b760b61a54b32e545186d945cb4edb6ab6389026
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-01 17:29:57 +0100

    added Brand Image and Name

--- components/Layout/Sidebar/Components/Brand.tsx
+++ components/Layout/Sidebar/Components/Brand.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
 import { FunctionComponent } from "react";
+
+import BrandIcon from "/public/favicon.ico"
 
 const Brand: FunctionComponent = () => {
     return (
-        <></>
+        <div className="w-full pl-5 pr-4 py-3 flex gap-3 items-center justify-start">
+            <div className="relative w-9 h-9">
+                <Image
+                    layout="fill"
+                    src={BrandIcon}
+                    alt="Brand Icon"
+                    objectFit="cover"
+                />
+            </div>
+            <span className="text-sm font-bold tracking-tight">
+                Grand Regal School
+            </span>
+        </div>
     );
 }
 
