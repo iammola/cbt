867e42d8359047b3c9e2b24f24aa0105abfe2536
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 17:38:33 +0100

    added `z-index`

--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -11,9 +11,9 @@ const Sidebar: FunctionComponent = () => {
 
     return (
         <aside
-            className={classNames("flex flex-col gap-5 items-center justify-start py-5 px-3 max-w-full h-screen bg-white rounded-lg shadow-sm relative", {
                 "w-screen sm:w-[20rem]": open,
                 "w-20 sm:w-[6.5rem]": open === false
+            className={classNames("flex flex-col gap-5 items-center justify-start py-5 px-3 max-w-full h-screen bg-white rounded-xl shadow-sm z-50 ", {
             })}
         >
             <Toggle
