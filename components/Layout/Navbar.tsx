a3db678b3d4813d41fe849aa176ff35319afa3d0
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 21:18:38 +0100

    imported Bell and CommentText icons

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,13 +1,20 @@
 import { FunctionComponent } from "react";
 
 import UserImage from "components/Misc/UserImage";
+import { BellIcon, CommentTextIcon } from "components/Misc/CustomIcons";
 
 const Navbar: FunctionComponent = () => {
     return (
         <header className="flex gap-4 items-center justify-between shadow-sm h-[6rem] w-full px-6 relative">
+            <div className="flex gap-5 flex-grow items-center justify-end pl-10 h-full">
+                <div className="p-3 rounded-full text-gray-600 hover:text-gray-800 hover:bg-gray-50 cursor-pointer">
+                    <CommentTextIcon className="w-5 h-5" />
+                </div>
+                <div className="p-3 rounded-full text-gray-600 hover:text-gray-800 hover:bg-gray-50 cursor-pointer">
+                    <BellIcon className="w-5 h-5" />
                 </div>
                 </div>
-                </div>
+            </div>
         </header>
     )
 }
