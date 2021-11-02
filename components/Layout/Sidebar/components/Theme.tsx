f915466efadc0093ab044c0c96cf21434bca3eed
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 13:16:34 +0100

    modified styles to add `z-index`

--- components/Layout/Sidebar/components/Theme.tsx
+++ components/Layout/Sidebar/components/Theme.tsx
@@ -5,8 +5,8 @@ import { MoonIcon, SunIcon } from "components/Misc/CustomIcons";
 
 const Theme: FunctionComponent<ThemeProps> = ({ open }) => {
     return (
-        <div className="flex items-center justify-between w-full h-12 p-1 rounded-full overflow-hidden bg-gray-100">
             <div className="flex gap-2 items-center justify-center flex-grow text-gray-800 h-full rounded-full cursor-pointer bg-white">
+        <div className="flex items-center justify-between w-full h-12 p-1 rounded-full overflow-hidden bg-gray-100 relative z-0">
                 <SunIcon className="w-5 h-5 flex-shrink-0" />
                 <span
                     className={classNames("text-sm", {
