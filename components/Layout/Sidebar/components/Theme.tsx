36b7997085013c80046dadfc29f2a8ae5f74b565
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 12:54:58 +0100

    added color to style

--- components/Layout/Sidebar/components/Theme.tsx
+++ components/Layout/Sidebar/components/Theme.tsx
@@ -5,14 +5,14 @@ import { MoonIcon, SunIcon } from "components/Misc/CustomIcons";
 const Theme: FunctionComponent = () => {
     return (
         <div className="flex items-center justify-between w-full h-12 p-1 rounded-full overflow-hidden bg-gray-100">
-            <div className="flex gap-2 items-center justify-center flex-grow h-full rounded-full cursor-pointer bg-white">
-                <SunIcon className="w-5 h-5 text-gray-800" />
+            <div className="flex gap-2 items-center justify-center flex-grow text-gray-800 h-full rounded-full cursor-pointer bg-white">
+                <SunIcon className="w-5 h-5" />
                 <span className="text-sm">
                     Light
                 </span>
             </div>
-            <div className="flex gap-2 items-center justify-center flex-grow h-full rounded-full cursor-pointer">
-                <MoonIcon className="w-5 h-5 text-gray-800" />
+            <div className="flex gap-2 items-center justify-center flex-grow text-gray-800 h-full rounded-full cursor-pointer">
+                <MoonIcon className="w-5 h-5" />
                 <span className="text-sm">
                     Dark
                 </span>
