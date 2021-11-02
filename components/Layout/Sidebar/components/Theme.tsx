b630e39e46773a0c89a06d5bde42010d822dcab1
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 12:58:45 +0100

    added `flex-shrink` class

--- components/Layout/Sidebar/components/Theme.tsx
+++ components/Layout/Sidebar/components/Theme.tsx
@@ -6,14 +6,14 @@ const Theme: FunctionComponent<ThemeProps> = ({ open }) => {
     return (
         <div className="flex items-center justify-between w-full h-12 p-1 rounded-full overflow-hidden bg-gray-100">
             <div className="flex gap-2 items-center justify-center flex-grow text-gray-800 h-full rounded-full cursor-pointer bg-white">
-                <SunIcon className="w-5 h-5" />
                 <span className="text-sm">
+                <SunIcon className="w-5 h-5 flex-shrink-0" />
                     Light
                 </span>
             </div>
             <div className="flex gap-2 items-center justify-center flex-grow text-gray-800 h-full rounded-full cursor-pointer">
-                <MoonIcon className="w-5 h-5" />
                 <span className="text-sm">
+                <MoonIcon className="w-5 h-5 flex-shrink-0" />
                     Dark
                 </span>
             </div>
