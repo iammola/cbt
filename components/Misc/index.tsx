b22e95e4ea096bdb0924a7cff3ad234e1376fc8b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 23:39:51 +0100

    exported `Badges`

--- components/Misc/index.tsx
+++ components/Misc/index.tsx
@@ -1,3 +1,4 @@
+export { default as Badges } from "./Badges";
 export { default as Divide } from "./Divide";
 export { default as Calendar } from "./Calendar";
 export { default as Schedule } from "./Schedule";
