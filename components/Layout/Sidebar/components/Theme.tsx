d304e2b909a57c4e68d0fb36fecfecdafcf162b4
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 12:52:51 +0100

    added elements

--- components/Layout/Sidebar/components/Theme.tsx
+++ components/Layout/Sidebar/components/Theme.tsx
@@ -2,7 +2,18 @@ import { FunctionComponent } from "react";
 
 const Theme: FunctionComponent = () => {
     return (
-        <></>
+        <div className="flex items-center justify-between w-full h-12 p-1 rounded-full overflow-hidden bg-gray-100">
+            <div className="flex gap-2 items-center justify-center flex-grow h-full rounded-full cursor-pointer bg-white">
+                <span className="text-sm">
+                    Light
+                </span>
+            </div>
+            <div className="flex gap-2 items-center justify-center flex-grow h-full rounded-full cursor-pointer">
+                <span className="text-sm">
+                    Dark
+                </span>
+            </div>
+        </div>
     );
 }
 
