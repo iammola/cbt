d302d5e99b8a5709c722218eb07ce00476f15e26
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 12:53:04 +0100

    imported icons

--- components/Layout/Sidebar/components/Theme.tsx
+++ components/Layout/Sidebar/components/Theme.tsx
@@ -1,14 +1,18 @@
 import { FunctionComponent } from "react";
 
+import { MoonIcon, SunIcon } from "components/Misc/CustomIcons";
+
 const Theme: FunctionComponent = () => {
     return (
         <div className="flex items-center justify-between w-full h-12 p-1 rounded-full overflow-hidden bg-gray-100">
             <div className="flex gap-2 items-center justify-center flex-grow h-full rounded-full cursor-pointer bg-white">
+                <SunIcon className="w-5 h-5 text-gray-800" />
                 <span className="text-sm">
                     Light
                 </span>
             </div>
             <div className="flex gap-2 items-center justify-center flex-grow h-full rounded-full cursor-pointer">
+                <MoonIcon className="w-5 h-5 text-gray-800" />
                 <span className="text-sm">
                     Dark
                 </span>
