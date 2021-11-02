36f97ce759edbf81d87303c184b5a6329f23154e
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-02 12:59:14 +0100

    added `hidden` class when sidebar closed

--- components/Layout/Sidebar/components/Theme.tsx
+++ components/Layout/Sidebar/components/Theme.tsx
@@ -1,19 +1,24 @@
 import { FunctionComponent } from "react";
 
+import { classNames } from "utils";
 import { MoonIcon, SunIcon } from "components/Misc/CustomIcons";
 
 const Theme: FunctionComponent<ThemeProps> = ({ open }) => {
     return (
         <div className="flex items-center justify-between w-full h-12 p-1 rounded-full overflow-hidden bg-gray-100">
             <div className="flex gap-2 items-center justify-center flex-grow text-gray-800 h-full rounded-full cursor-pointer bg-white">
-                <span className="text-sm">
                 <SunIcon className="w-5 h-5 flex-shrink-0" />
+                <span className={classNames("text-sm", {
+                    "hidden": open === false
+                })}>
                     Light
                 </span>
             </div>
             <div className="flex gap-2 items-center justify-center flex-grow text-gray-800 h-full rounded-full cursor-pointer">
-                <span className="text-sm">
                 <MoonIcon className="w-5 h-5 flex-shrink-0" />
+                <span className={classNames("text-sm", {
+                    "hidden": open === false
+                })}>
                     Dark
                 </span>
             </div>
