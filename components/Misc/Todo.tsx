91d20da972d0c5f6ba15bcd4285c7115f87d0622
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-03 15:55:35 +0100

    added Todo wrapping element

--- components/Misc/Todo.tsx
+++ components/Misc/Todo.tsx
@@ -1,5 +1,10 @@
 import { FunctionComponent } from "react";
 const Todo: Todo = ({ items }) => {
+    return (
+        <div className="flex flex-wrap flex-row items-start justify-start content-start gap-x-4 gap-y-3 w-full h-full">
+        </div>
+    );
+}
 
     return (
         <></>
