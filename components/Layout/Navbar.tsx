ca6521a1090c0f9737860fc1496c9fa6deb70f18
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-03 03:48:17 +0100

    modified styles to add `background-color`

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: FunctionComponent = () => {
     }
 
     return (
-        <header className="flex gap-4 items-center justify-between shadow-sm h-[6rem] w-full px-6 relative">
+        <header className="flex gap-4 items-center justify-between bg-white shadow-sm h-[6rem] w-full px-6 relative">
             <div className="flex gap-5 flex-grow items-center justify-end pl-10 h-full relative">
                 <div className="p-3 rounded-full text-gray-600 hover:text-gray-800 hover:bg-gray-50 cursor-pointer">
                     <CommentTextIcon className="w-5 h-5" />
