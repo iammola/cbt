484b769b22a03a35c7fe956a1dc802228dbd2a1b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-03 23:34:26 +0100

    added GET function

--- pages/api/teachers/[id]/subjects/extend.ts
+++ pages/api/teachers/[id]/subjects/extend.ts
@@ -1,7 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { ClassModel, TeacherModel } from "db/models";
+
 import type { RouteResponse } from "types";
+
+async function getExtendedTeacherSubjects(id: string, select: string): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const teacher = await TeacherModel.findById(id).populate('subjects', select).select('-_id subjects').lean();
+        const classes = await ClassModel.find({}, {
+            _id: 0, name: 1,
+            subjects: teacher?.subjects.map(({ _id }) => _id) ?? []
+        }).lean();
+
+        const data = classes.map(({ name, ...item }) => ({
+            name,
+            subjects: item.subjects.map(subject => teacher?.subjects.find(({ _id }) => _id.equals(subject)))
+        }));
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data,
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
