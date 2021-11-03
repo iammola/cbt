fcef89f60adb7062ff721f5749b2ee459096e73f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-03 15:58:38 +0100

    exported Todo

--- components/Misc/index.tsx
+++ components/Misc/index.tsx
@@ -1,3 +1,4 @@
+export { default as Todo } from "./Todo";
 export { default as Badges } from "./Badges";
 export { default as Divide } from "./Divide";
 export { default as Calendar } from "./Calendar";
