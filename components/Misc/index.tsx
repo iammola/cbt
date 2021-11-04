b710cb126b53b23381d1f8e43a41fdb06620bb09
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-04 14:56:40 +0100

    removed Calendar export

--- components/Misc/index.tsx
+++ components/Misc/index.tsx
@@ -2,6 +2,5 @@ export { default as Todo } from "./Todo";
 export { default as Badges } from "./Badges";
 export { default as Divide } from "./Divide";
 export { default as Subjects } from "./Subjects";
-export { default as Calendar } from "./Calendar";
 export { default as Schedule } from "./Schedule";
 export { default as UserImage } from "./UserImage";
