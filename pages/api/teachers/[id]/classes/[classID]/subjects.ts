8cb09e4d51e5bf0d9ec9d39ddc6652dfe6b4a052
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-05 07:35:29 +0100

    called GET function

--- pages/api/teachers/[id]/classes/[classID]/subjects.ts
+++ pages/api/teachers/[id]/classes/[classID]/subjects.ts
@@ -39,7 +39,7 @@ export default async function handler({ query, method }: NextApiRequest, res: Ne
     if (allowedMethods !== method) {
         res.setHeader("Allow", allowedMethods);
         [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
-    }
+    } else [success, status, message] = await getTeacherClassSubject(query.id, query.classID, query.select);
 
     if (typeof message !== "object") message = { message };
 
