e2e2c7811660ab6c5d04cb17ba5d902f2ceb6176
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-05 07:33:59 +0100

    added GET function

--- pages/api/teachers/[id]/classes/[classID]/subjects.ts
+++ pages/api/teachers/[id]/classes/[classID]/subjects.ts
@@ -1,7 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { SubjectModel } from "db/models";
+
 import type { RouteResponse } from "types";
+
+async function getTeacherClassSubject(id: any, classID: any, select: any): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const subjects = await SubjectModel.find({
+            class: classID,
+            "subjects.teacher": id
+        }, select);
+
+        const data = subjects.map(({ subjects }) => subjects.filter(({ teachers }) => teachers.find(teacher => teacher.toString() === id)).map(({ teachers, ...item }) => item)).flat();
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data,
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ query, method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
