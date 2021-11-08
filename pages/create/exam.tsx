630a3ccdfe2632ebc209fdea0e77b1338767f6e8
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 00:21:16 +0100

    set `modified` exam state to true

--- pages/create/exam.tsx
+++ pages/create/exam.tsx
@@ -114,6 +114,7 @@ const CreateQuestions: NextPage = () => {
         const replace = count === 0 ? { ...recordTemplate } : data;
         items.splice(pos, count, replace);
         setQuestions(items.filter(Boolean));
+        setExamState({ ...examState, modified: true });
     }
 
     return (
