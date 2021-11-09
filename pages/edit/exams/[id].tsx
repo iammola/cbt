7b46e225e6e44191b05565e0f819e83fa037ee8b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 23:44:52 +0100

    added `useRouter` and `useSWRImmutable` to fetch exam data

--- pages/edit/exams/[id].tsx
+++ pages/edit/exams/[id].tsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import useSWRImmutable from "swr/immutable";
 
 const CreateExam: NextPage = () => {
+    const router = useRouter();
+    const { data: exam } = useSWRImmutable(router.query.id !== undefined ? `/api/exams/${router.query.id}/edit/` : null, url => url !== null && fetch(url).then(res => res.json()));
+
     return (
         <>
             <Head>
