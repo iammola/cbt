d97d3ec3f57d1858a356eb1c020513f1f7da16d8
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 20:45:14 +0100

    initial commit of Exam Edit page

---
+++ pages/edit/exams/[id].tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import type { NextPage } from "next";
+
+const CreateExam: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Edit Exam | CBT | Grand Regal School</title>
+                <meta name="description" content="Exam Editing | GRS CBT" />
+            </Head>
+        </>
+    );
+}
+
+export default CreateExam;
