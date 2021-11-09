963f20d1355c138eb904242e22357fb974aa3c7f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 21:45:28 +0100

    created `SubjectsSchema` from `SubjectSchema`

--- db/models/Subject.ts
+++ db/models/Subject.ts
@@ -1,30 +1,30 @@
 import { Schema, Model, model, models } from "mongoose";
 
-import type { SubjectRecord } from "types";
+import type { SubjectRecord, SubjectsRecord } from "types";
 
 const SubjectSchema = new Schema<SubjectRecord>({
+    name: {
+        type: String,
+        required: [true, 'Subject name required'],
+        unique: true,
+        trim: true,
+    }, alias: {
+        type: String,
+        required: [true, 'Subject alias required'],
+        unique: true,
+        trim: true,
+    }, teachers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Teacher',
+    }]
+});
+
+const SubjectsSchema = new Schema<SubjectsRecord>({
     class: {
         type: Schema.Types.ObjectId,
         required: [true, 'Class needed'],
         unique: true,
-    }, subjects: {
-        type: [{
-            name: {
-                type: String,
-                required: [true, 'Subject name required'],
-                unique: true,
-                trim: true,
-            }, alias: {
-                type: String,
-                required: [true, 'Subject alias required'],
-                unique: true,
-                trim: true,
-            }, teachers: [{
-                type: Schema.Types.ObjectId,
-                ref: 'Teacher',
-            }]
-        }]
-    }
+    }, subjects: [SubjectSchema]
 });
 
 export const SubjectModel = models.Subject as Model<SubjectRecord> ?? model('Subject', SubjectSchema);
