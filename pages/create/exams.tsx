68efcd66a1982f68bd1d357cec64ad3729821f05
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 14:22:09 +0100

    moved component to separate file

--- pages/create/exams.tsx
+++ pages/create/exams.tsx
@@ -1,15 +1,12 @@
-import useSWR from "swr";
 import Head from "next/head";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
-import { CheckIcon, ChevronRightIcon } from "@heroicons/react/solid";
 import { FormEvent, FunctionComponent, useMemo, useState } from "react";
 import { CogIcon, CheckCircleIcon, ExclamationCircleIcon, XCircleIcon, BellIcon, XIcon, ArrowSmUpIcon } from "@heroicons/react/outline";
 
 import { useNotifications } from "components/Misc/Notification";
 
-import { LoadingIcon } from "components/Misc/Icons";
 import { Modal as ExamModal, Question } from "components/Exam";
 
 import type { CreateQuestion } from "types";
@@ -226,74 +223,6 @@ const CreateExams: NextPage = () => {
             <Notifications />
         </>
     );
-}
-
-const Bar: FunctionComponent<BarProps> = ({ exam, save, modified, saved, uploading, uploaded }) => {
-    const { data: currentSession } = useSWR('/api/sessions/current/', url => fetch(url).then(res => res.json()));
-
-    return (
-        <div className="flex items-center justify-end gap-6 w-full bg-white py-5 px-8 sticky left-0 top-0 rounded-b-lg drop-shadow-sm">
-            <div className="hidden md:flex items-center justify-start gap-2 flex-grow text-gray-400 w-full text-sm font-medium">
-                <span className="w-max block truncate">
-                    {currentSession !== undefined ? (
-                        currentSession.data !== null ? (
-                            <>
-                                <span className="inline-block sm:hidden">
-                                    {currentSession.data.alias}{' '}
-                                    {currentSession.data.terms[0].alias}
-                                </span>
-                                <span className="hidden sm:inline-block">
-                                    {currentSession.data.name}{' '}
-                                    {currentSession.data.terms[0].name}
-                                </span>{' '}Term
-                            </>
-                        ) : "No Current Session"
-                    ) : "Loading Session"}
-                </span>
-                <ChevronRightIcon className="w-5 h-5 text-gray-500" />
-                <span className="w-max block truncate">{exam?.class ?? "Select Class"}</span>
-                <ChevronRightIcon className="w-5 h-5 text-gray-500" />
-                <span className="w-max block truncate">{exam?.subject ?? "Select Subject"}</span>
-                <ChevronRightIcon className="w-5 h-5 text-gray-500" />
-                <span className="text-gray-600">Questions</span>
-            </div>
-            <button
-                type="button"
-                onClick={() => save()}
-                title={modified === true ? "Changes made. Don't forget to save" : saved === true ? "No changes made." : ""}
-                className="flex items-center justify-center gap-2 py-3 px-8 tracking-wider text-xs font-medium bg-indigo-100 hover:bg-indigo-200 text-indigo-700 rounded-md shadow-sm"
-            >
-                {modified === true && (
-                    <span className="w-2 h-2 rounded-full bg-indigo-300" />
-                )}
-                {saved === true && (
-                    <CheckIcon className="w-5 h-5" />
-                )}
-                Save
-            </button>
-            <button
-                type="submit"
-                className="flex items-center justify-center gap-2 py-3 px-8 tracking-wider text-xs font-medium bg-indigo-500 hover:bg-indigo-600 text-white rounded-md shadow-sm"
-            >
-                {uploading === true && (
-                    <LoadingIcon className="animate-spin w-5 h-5" />
-                )}
-                {uploaded === true && (
-                    <CheckIcon className="w-5 h-5" />
-                )}
-                Submit
-            </button>
-        </div>
-    );
-}
-
-type BarProps = {
-    save(): void;
-    saved: boolean;
-    modified: boolean;
-    uploaded: boolean;
-    uploading: boolean;
-    exam?: { class: string; subject: string };
 }
 
 export default CreateExams;
