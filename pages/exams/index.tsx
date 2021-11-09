7fbc1e1dee17857aad994740c3ade0969a9f5e00
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 01:59:11 +0100

    initial commit of Exams page

---
+++ pages/exams/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import type { NextPage } from "next";
+
+const Exams: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Exams | CBT | Grand Regal School</title>
+                <meta name="description" content="Registered Exams | GRS CBT" />
+            </Head>
+        </>
+    );
+}
+
+export default Exams;
