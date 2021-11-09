4ee50d6a42304794ee4f5f6402c8241c963bf4a7
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 04:28:47 +0100

    changed `allowedMethods` to GET alone

--- pages/api/teachers/[id]/exams/index.ts
+++ pages/api/teachers/[id]/exams/index.ts
@@ -5,9 +5,9 @@ import type {RouteResponse } from "types";
 
 export default async function handler({ method, query, body }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
-    const allowedMethods = ["POST", "GET"];
+    const allowedMethods = "GET";
 
-    if (allowedMethods.includes(method ?? '') === false) {
+    if (allowedMethods !== method) {
         res.setHeader("Allow", allowedMethods);
         [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
     }
