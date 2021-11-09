8c71584cf2ea6248bcd2a447c29cded59039f0dd
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 14:43:52 +0100

    removed unused `next/head` import

--- components/Exam/Form.tsx
+++ components/Exam/Form.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
 import { FormEvent, FunctionComponent, useMemo, useState } from "react";
