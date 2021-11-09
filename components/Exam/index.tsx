57a8bd45835965091227abe7373d0c9b443a64ec
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-09 14:23:56 +0100

    exported `Form` and `Bar` components

--- components/Exam/index.tsx
+++ components/Exam/index.tsx
@@ -1,3 +1,5 @@
+export { default as Bar } from "./Bar";
+export { default as Form } from "./Form";
 export { default as Modal } from "./Modal";
 export { default as Answer } from "./Answer";
 export { default as Question } from "./Question";
