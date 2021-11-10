edda4e3567f652a5d8d921bc70d62e41d3df0317
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-10 02:04:34 +0100

    imported `Form` and `Loader` components

--- pages/edit/exams/[id].tsx
+++ pages/edit/exams/[id].tsx
@@ -3,6 +3,8 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import useSWRImmutable from "swr/immutable";
 
+import { Form, Loader } from "components/Exam";
+
 const CreateExam: NextPage = () => {
     const router = useRouter();
     const { data: exam } = useSWRImmutable(router.query.id !== undefined ? `/api/exams/${router.query.id}/edit/` : null, url => url !== null && fetch(url).then(res => res.json()));
@@ -13,6 +15,8 @@ const CreateExam: NextPage = () => {
                 <title>Edit Exam | CBT | Grand Regal School</title>
                 <meta name="description" content="Exam Editing | GRS CBT" />
             </Head>
+            <Form {...exam?.data} />
+            <Loader show={exam === undefined} />
         </>
     );
 }
