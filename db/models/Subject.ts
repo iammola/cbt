fb6f3bcbaed9745a2554aa310fc68611dd52518f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-10 06:27:23 +0100

    added `ref` property to `SubjectsSchema`

--- db/models/Subject.ts
+++ db/models/Subject.ts
@@ -24,6 +24,7 @@ const SubjectsSchema = new Schema<SubjectsRecord>({
         type: Schema.Types.ObjectId,
         required: [true, 'Class needed'],
         unique: true,
+        ref: 'Class',
     }, subjects: [SubjectSchema]
 });
 
