7420b51af7584677a22c2c951c231df589a05388
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-12 19:22:03 +0100

    added `QuestionSchema`

--- db/models/Exam.ts
+++ db/models/Exam.ts
@@ -1,6 +1,29 @@
 import { Schema, Model, model, models } from "mongoose";
 
-import type { ExamRecord } from "types";
+import type { ExamRecord, QuestionRecord } from "types";
+
+const QuestionSchema = new Schema<QuestionRecord>({
+    question: {
+        type: String,
+        trim: true,
+        required: [true, 'Question required'],
+    }, minLength: {
+        type: Number,
+        required: [function (this: QuestionRecord) {
+            return this.type === "Long Answer";
+        }, "Long Answer Questions require a minimum length"]
+    }, maxLength: {
+        type: Number,
+        default: undefined,
+    }, type: {
+        type: String,
+        required: [true, "Question type required"],
+        enum: {
+            message: "Invalid Question type",
+            values: ["Multiple choice", "Checkboxes", "Short Answer", "Long Answer"]
+        }
+    },
+});
 
 const ExamSchema = new Schema<ExamRecord>({
     duration: {
