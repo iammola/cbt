79d564c378d29107fec44f0b77edf429f2804c98
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-13 19:11:28 +0100

    initial commit of WriteExam component

---
+++ pages/write/exams/[id].tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+const WriteExam: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Subject | Event | Exam | CBT | Grand Regal School</title>
+                <meta name="description" content="Subject Exam | GRS CBT" />
+            </Head>
+        </>
+    );
+}
+
+export default WriteExam;
