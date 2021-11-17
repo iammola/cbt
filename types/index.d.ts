6e3c7d33c619e3e9f2a612896e6b7e092c76f1e2
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-17 08:58:41 +0100

    added `ExamDetails` type

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,10 +7,15 @@ export type CreateQuestion<P = false, I = false> = Omit<QuestionRecord<P, I>, 'a
     answers: AnswerRecord[];
 };
 
-export type ExamDetails = {
-    class: string;
-    subject: string;
-    details: Pick<ExamRecord, 'duration' | 'SubjectID'>;
+export type ExamData = {
+    _id: string;
+    questions: QuestionRecord<true>[];
+    details: Pick<ExamRecord, 'duration' | 'instructions' | 'subjectId'> & {
+        name: {
+            class: string;
+            subject: string;
+        };
+    };
 }
 
 export type RouteResponse = [boolean, number, string | Record<string, any> & { error?: unknown, message: string }];
