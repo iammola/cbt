75e1fa4f75aa2a20349e304275d1a2bbd1527dcc
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-17 08:49:29 +0100

    used `AnswerRecord` to generate `StudentAnswer` props

--- types/components.d.ts
+++ types/components.d.ts
@@ -104,9 +104,7 @@ type StudentQuestionProps = {
     answers: Pick<StudentAnswerProps, 'id' | 'answer'>[];
 }
 
-type StudentAnswerProps = {
-    id: string;
-    answer: string;
+type StudentAnswerProps = Omit<AnswerRecord<true>, 'isCorrect'> & {
     selected: boolean;
     handleSelect(id: string): void;
 }
