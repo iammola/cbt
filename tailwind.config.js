327c198c01febc7ae3a804388178dcacebede24d
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-18 15:11:13 +0100

    `darkMode` set to `false` works the same as media with JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 
 module.exports =  {
     content: ["pages/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
-    darkMode: false, // or 'media' or 'class'
     theme: {},
     variants: {},
     plugins: [],
