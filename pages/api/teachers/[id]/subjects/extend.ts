df7239ab7acac6144fdb6c1a46537972fdc730f6
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-18 02:03:40 +0100

    used populated class to format return data

--- pages/api/teachers/[id]/subjects/extend.ts
+++ pages/api/teachers/[id]/subjects/extend.ts
@@ -11,16 +11,13 @@ async function getExtendedTeacherSubjects(id: any): Promise<RouteResponse> {
     let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
 
     try {
-        const classes = await ClassModel.find({ _id: subjects.map(item => item.class) }, 'name').lean();
-
-        const data = classes.map(({ name, ...classItem }) => ({
-            name,
-            subjects: subjects.find(item => (classItem as any)._id.equals(item.class))?.subjects.filter(({ teachers }) => teachers.find(teacher => teacher.toString() === id)).map(({ teachers, ...item }) => item) ?? [],
-        }));
         const subjects: SubjectsRecord<true>[] = await SubjectsModel.find({ "subjects.teachers": id }, "-_id").populate('class', 'name').lean();
 
         [success, status, message] = [true, StatusCodes.OK, {
-            data,
+            data: subjects.map(cur => ({
+                name: cur.class.name,
+                subjects: cur.subjects.filter(({ teachers }) => teachers.find(teacher => teacher.equals(id))).map(({ teachers, ...item }) => item)
+            })),
             message: ReasonPhrases.OK
         }];
     } catch (error: any) {
