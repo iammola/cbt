afcb9e0068727db5f997b4bd6e890772401f4521
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-20 20:24:02 +0100

    initial commit of Modal component

---
+++ components/Exam/Student/Modal.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+import { Transition, Dialog } from "@headlessui/react";
+
+const Modal: FunctionComponent = () => {
+    return (
+        <></>
+    );
+}
+
+export default Modal;
