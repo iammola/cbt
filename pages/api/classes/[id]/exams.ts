1cc0d1766326e0ec9366e8053e9c2ef35d7c3e6f
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-24 03:23:43 +0100

    added `getExams` implementation

--- pages/api/classes/[id]/exams.ts
+++ pages/api/classes/[id]/exams.ts
@@ -1,9 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { ExamModel, SubjectsModel } from "db/models";
+
 import type { RouteResponse } from "types";
 
 export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
+async function getExams(classId: any): Promise<RouteResponse> {
+    await connect();
+    let [success, status, message]: RouteResponse = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const data = await SubjectsModel.findOne({ class: classId }, '-subjects.teachers').lean();
+        if (data === null) throw new Error('Class does not exist');
+
+        const examsRecord = await ExamModel.find({ subjectId: data.subjects.map(({ _id }) => _id) }, '-_id subjectId').lean();
+        const exams = examsRecord.map(({ subjectId }) => data.subjects.find(({ _id }) => _id.equals(subjectId)));
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data: { exams },
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
+
     let [success, status, message]: RouteResponse = [false, StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST];
     const allowedMethods = "GET";
 
