be7f8ac6448f97d2f81154adbb0706d2ece05078
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-27 19:18:08 +0100

    added `getEventsRange` function

--- pages/api/events/range.ts
+++ pages/api/events/range.ts
@@ -1,8 +1,50 @@
+import { format } from "date-fns";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { EventModel, SubjectsModel } from "db/models";
+
 import type { EventsRangeGETData } from "types/api/events";
 import type { EventRecord, ServerResponse, SubjectsRecord } from "types";
+
+async function getEventsRange(from: any, to: any): Promise<ServerResponse<EventsRangeGETData>> {
+    await connect();
+    let [success, status, message]: ServerResponse<EventsRangeGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const events: EventRecord<true>[] = await EventModel.find({
+            date: {
+                $lte: new Date(+to),
+                $gte: new Date(+from)
+            }
+        }, 'from exams').populate('exams', 'subjectId').lean();
+
+        const data = await Promise.all(events.map(async ({ from, exams }) => ({
+            date: new Date(from),
+            time: format(new Date(from), 'h:mm a'),
+            events: (await Promise.all(exams.map(async ({ subjectId }) => {
+                const record: SubjectsRecord<true> | null = await SubjectsModel.findOne({
+                    "subjects._id": subjectId
+                }, 'class subjects.name.$').populate('class', 'alias').lean();
+
+                return record === null ? '' : `${record.class.alias} ${record.subjects[0].name}`;
+            }))).filter(Boolean)
+        })));
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data,
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ query, method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: ServerResponse<EventsRangeGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
