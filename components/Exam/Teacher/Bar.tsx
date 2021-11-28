369afcdfcf1e1e40a75f2de820e3ab7946bdc8f3
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-28 10:22:38 +0100

    used `useSWRImmutable`

--- components/Exam/Teacher/Bar.tsx
+++ components/Exam/Teacher/Bar.tsx
@@ -8,7 +8,7 @@ import type { RouteData, TeacherBarProps } from "types";
 import type { SessionCurrentGETData } from "types/api/sessions";
 
 const Bar: FunctionComponent<TeacherBarProps> = ({ exam, save, modified, saved, uploading, uploaded }) => {
-    const { data: currentSession } = useSWR<RouteData<SessionCurrentGETData>>('/api/sessions/current/', url => fetch(url).then(res => res.json()));
+    const { data: currentSession } = useSWRImmutable<RouteData<SessionCurrentGETData>>('/api/sessions/current/', url => fetch(url).then(res => res.json()));
 
     return (
         <div className="flex items-center justify-end gap-6 w-full bg-white py-5 px-8 sticky left-0 top-0 rounded-b-lg drop-shadow-sm">
