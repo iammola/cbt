305622cee74a35cc0fa388fc550c0316d843b548
Ademola Adedeji <a.mola.dev@gmail.com>
2021-11-29 08:03:50 +0100

    added Student Loader and Modal components

--- components/Exam/Student/Loader.tsx
+++ components/Exam/Student/Loader.tsx
@@ -1,12 +1,12 @@
 import { Fragment, FunctionComponent } from "react";
 import { Transition, Dialog } from "@headlessui/react";
 
-const Modal: FunctionComponent<{ show: boolean; close(): void; confirm(): void }> = ({ show, close, confirm }) => {
+const Modal: FunctionComponent<{ show: boolean }> = ({ show }) => {
     return (
         <Transition show={show} appear as={Fragment}>
             <Dialog
                 as="section"
-                onClose={close}
+                onClose={() => { }}
                 className="flex items-center justify-center fixed z-50 inset-0 h-screen w-full p-8 backdrop-blur-sm"
             >
                 <Transition.Child
@@ -31,28 +31,14 @@ const Modal: FunctionComponent<{ show: boolean; close(): void; confirm(): void }
                 >
                     <div className="flex flex-col items-center gap-7 rounded-3xl shadow-lg p-12 bg-white w-full sm:w-[50rem]">
                         <Dialog.Title className="text-4xl text-gray-800 font-bold tracking-tight text-center pb-4">
-                            <span className="text-rose-500">Submit</span>{' '}
-                            <span>the exam?</span>
+                            <span>Loading</span>{' '}
+                            <span className="text-sky-500">Exam</span>{' '}
+                            <span>🎲</span>
                         </Dialog.Title>
-                        <Dialog.Description>
-                            <span>Are you sure you want to submit</span>{' '}
-                        </Dialog.Description>
-                        <div className="flex gap-5 item-center justify-end mt-2">
-                            <button
-                                type="button"
-                                onClick={confirm}
-                                className="inline-flex justify-center px-4 py-2 text-xs font-medium text-rose-900 bg-rose-100 border border-transparent rounded-md hover:bg-rose-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                            >
-                                Submit
-                            </button>
-                            <button
-                                type="button"
-                                onClick={close}
-                                className="inline-flex justify-center px-4 py-2 text-xs font-medium text-gray-900 bg-gray-100 border border-transparent rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-                            >
-                                Cancel
-                            </button>
-                        </div>
+                        <div
+                            style={{ borderRightColor: 'rgb(14, 165, 233)' }}
+                            className="w-16 h-16 rounded-full border-2 border-gray-200 animate-spin"
+                        />
                     </div>
                 </Transition.Child>
             </Dialog>
