e45ea03feccafcb34468a5ea7918438194026103
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-01 10:23:10 +0100

    removed questions grid

--- pages/exams/write/[id].tsx
+++ pages/exams/write/[id].tsx
@@ -133,13 +133,13 @@ const WriteExam: NextPage = () => {
                     onSubmit={() => setSubmitting(true)}
                 />
                 <div className="flex grow gap-6 items-center justify-center w-full h-full pt-6 px-12 bg-gray-50">
-                    <div className="flex flex-col items-start justify-start h-full w-[18rem] py-8">
+                    {/* <div className="flex flex-col items-start justify-start h-full w-[18rem] py-8">
                         <Grid
                             questions={exam?.data?.questions?.map(({ _id }) => ({
                                 answered: !!answered[_id.toString()],
                             })) ?? []}
                         />
-                    </div>
+                    </div> */}
                     <div className="grow h-full px-14 py-8">
                         <div className="max-w-5xl h-full space-y-10">
                             {exam?.data.questions.map((question, questionIdx) => (
