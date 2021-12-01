5d2e87dd8e07e8e830bb3b8b3a7b17a19099bec1
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-01 06:01:35 +0100

    added tabs

--- components/Home/Student/index.tsx
+++ components/Home/Student/index.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 
 import Exams from "./Exams";
+import { classNames } from "utils";
 
 const Home: FunctionComponent = () => {
     return (
@@ -22,6 +23,25 @@ const Home: FunctionComponent = () => {
                 </button>
             </div>
             <div className="px-10 md:px-16 xl:px-20 w-full">
+                <div className="flex gap-12 mt-10 border-b-2 w-max min-w-full mb-8">
+                    {["Exams", "Results"].map(label => (
+                        <div
+                            key={label}
+                            className={classNames("group flex items-center relative px-2 pb-3 gap-3.5 cursor-pointer after:absolute after:w-full after:h-0.5 after:left-0 after:-bottom-0.5 after:transition-all hover:after:bg-gray-600", {
+                                "after:bg-gray-600": false
+                            })}
+                        >
+                            <span
+                                className={classNames("font-semibold w-max group-hover:text-gray-800 transition-all", {
+                                    "text-gray-600": true,
+                                    "text-gray-800": false
+                                })}
+                            >
+                                {label}
+                            </span>
+                        </div>
+                    ))}
+                </div>
                 <Exams />
             </div>
         </section>
