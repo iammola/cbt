e6fa6ffc037e7a8fdaf97e50e43a19262c3a04e5
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-05 11:19:00 +0100

    added Head elements

--- pages/results/scheme/create.tsx
+++ pages/results/scheme/create.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import { NextPage } from "next";
 
 const CreateScheme: NextPage = () => {
     return (
-        <></>
+        <>
+            <Head>
+                <title>Create Scheme | Portal | Grand Regal School</title>
+                <meta name="description" content="Create Scheme | GRS Portal" />
+            </Head>
+        </>
     );
 }
 
