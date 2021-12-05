e4af9c9ad59b96172e2a4f76b1eff873ff9579a8
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-05 14:11:25 +0100

    added `ResultModel`

---
+++ db/models/Result.ts
@@ -0,0 +1,31 @@
+import { Schema, Model, model, models } from "mongoose";
+
+import type { ResultRecord } from "types";
+
+const ResultSchema = new Schema<ResultRecord>({
+    student: {
+        type: Schema.Types.ObjectId,
+        unique: true,
+        ref: 'Student',
+        required: [true, "Student ID required"]
+    }, data: {
+        _id: false,
+        type: [{
+            subject: {
+                type: Schema.Types.ObjectId
+            }, scores: {
+                _id: false,
+                type: [{
+                    fieldId: {
+                        type: Schema.Types.ObjectId
+                    }, score: {
+                        min: 0,
+                        type: Number,
+                    }
+                }],
+            }
+        }],
+    }
+});
+
+export const ResultModel = models.Result as Model<ResultRecord> ?? model('Result', ResultSchema);
