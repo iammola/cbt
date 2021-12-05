4dfd3fd6ff34a570668d227437f0ad4764338a3b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-05 10:12:58 +0100

    added page Head

--- pages/results/index.tsx
+++ pages/results/index.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import type { NextPage } from "next";
 
 const Results: NextPage = () => {
     return (
-        <></>
+        <>
+            <Head>
+                <title>Results | Portal | Grand Regal School</title>
+                <meta name="description" content="Results | GRS Portal" />
+            </Head>
+        </>
     );
 }
 
