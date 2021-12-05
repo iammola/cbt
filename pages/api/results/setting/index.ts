4a3c395fe52010797ee42ca984298f188f1ba570
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-05 20:00:13 +0100

    initial commit of Result Settings API route

---
+++ pages/api/results/setting/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+import type { ServerResponse } from "types";
+import type { ResultsSettingPOSTData } from "types/api/results";
+
+export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
+    let [success, status, message]: ServerResponse<ResultsSettingPOSTData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+    const allowedMethods = ["POST", "GET"];
+
+    if (allowedMethods.includes(method ?? '') === false) {
+        res.setHeader("Allow", allowedMethods);
+        [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
+    }
+
+    if (typeof message !== "object") message = { message, error: message };
+
+    res.status(status).json({ success, ...message });
+}
---
+++ types/api/results.d.ts
@@ -0,0 +1,3 @@
+export type ResultsSettingPOSTData = {
+    ok: boolean;
+}
