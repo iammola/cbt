d4e50175eaefa47fda79bde93f0a5d3885eee44e
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-06 14:01:24 +0100

    added `createResultSetting` function

--- pages/api/results/setting/index.ts
+++ pages/api/results/setting/index.ts
@@ -1,8 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
-import type { ServerResponse } from "types";
+import { connect } from "db";
+import { ClassModel, SessionModel } from "db/models";
+
+import type { ClassResultTemplate, ServerResponse } from "types";
 import type { ResultsSettingPOSTData } from "types/api/results";
+
+async function createResultSetting(body: Omit<ClassResultTemplate, 'term'> & { class: string }): Promise<ServerResponse<ResultsSettingPOSTData>> {
+    await connect();
+    let [success, status, message]: ServerResponse<ResultsSettingPOSTData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const classRecord = await ClassModel.findById(body.class, 'resultTemplate').lean();
+        if (classRecord === null) throw new Error("Class does not exist");
+
+        const currentSession = await SessionModel.findOne({ current: true, "terms.current": true }, 'terms._id.$').lean();
+
+        const data = await ClassModel.updateOne({ _id: body.class }, {
+            $push: {
+                resultTemplate: {
+                    session: currentSession?._id ?? "",
+                    terms: [{
+                        fields: body.fields,
+                        scheme: body.scheme,
+                        term: currentSession?.terms[0]._id,
+                    }]
+                }
+            }
+        }, {
+            runValidators: true,
+            lean: true, fields: "_id",
+        });
+
+        [success, status, message] = [true, StatusCodes.CREATED, {
+            data: { ok: data.acknowledged },
+            message: ReasonPhrases.CREATED
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: ServerResponse<ResultsSettingPOSTData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
