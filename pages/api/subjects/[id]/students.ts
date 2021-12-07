8c18dbb7c3ae08a70b728adeeb4980c98290cfa5
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-07 08:55:01 +0100

    added `getStudents` function and query

--- pages/api/subjects/[id]/students.ts
+++ pages/api/subjects/[id]/students.ts
@@ -1,10 +1,49 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { SessionModel, StudentModel } from "db/models";
+
 import type { ServerResponse } from "types";
 import type { SubjectStudentsGETData } from "types/api/subjects";
 
 export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
+async function getStudents(subjectId: any): Promise<ServerResponse<SubjectStudentsGETData>> {
+    await connect();
+    let [success, status, message]: ServerResponse<SubjectStudentsGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const currentSession = await SessionModel.findOne({ current: true, "terms.current": true }, 'terms._id.$').lean();
+        if (currentSession === null) throw new Error("Current session not found");
+
+        const students = await StudentModel.find({
+            academic: {
+                $elemMatch: {
+                    session: currentSession._id,
+                    terms: {
+                        $elemMatch: {
+                            term: currentSession.terms[0]._id,
+                            subjects: subjectId,
+                        }
+                    }
+                }
+            }
+        }, 'name').lean();
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data: { students },
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
+
     let [success, status, message]: ServerResponse<SubjectStudentsGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
     const allowedMethods = "GET";
 
