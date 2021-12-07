4fcde38b1a18af9109e3ed28a46949f35318c0fd
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-07 09:16:16 +0100

    initial commit of Student Subject Result API route

---
+++ pages/api/students/[id]/results/[subjectId].ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+import type { ServerResponse } from "types";
+
+export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
+    let [success, status, message]: ServerResponse<{}> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+    const allowedMethods = ["GET", "POST"];
+
+    if (allowedMethods.includes(method ?? '') === false) {
+        res.setHeader("Allow", allowedMethods);
+        [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
+    }
+
+    if (typeof message !== "object") message = { message, error: message };
+
+    res.status(status).json({ success, ...message });
+}
