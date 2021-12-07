2549eff90867ba42027137679afb64dd3c700357
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-07 08:48:53 +0100

    moved file

--- pages/api/subjects/[id]/students.ts
+++ pages/api/subjects/[id]/students.ts
@@ -3,11 +3,11 @@ import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
 import type { ServerResponse } from "types";
 
-export default async function handler({ body, method }: NextApiRequest, res: NextApiResponse) {
+export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: ServerResponse<{}> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
-    const allowedMethods = ["GET", "POST"];
+    const allowedMethods = "GET";
 
-    if (allowedMethods.includes(method ?? '') === false) {
+    if (allowedMethods !== method) {
         res.setHeader("Allow", allowedMethods);
         [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
     }
