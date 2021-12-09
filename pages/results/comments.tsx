cc5412043f31e9c8c8455b817e9b9ccd320a4821
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-09 17:32:42 +0100

    initial commit of Result Comments page

---
+++ pages/results/comments.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import type { NextPage } from "next";
+
+const Comments: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Comments | CBT | Grand Regal School</title>
+                <meta name="description" content="Comments | GRS CBT" />
+            </Head>
+        </>
+    );
+}
+
+export default Comments;
