79eac99861988403dc5e1de3c7cd114c68d87dcf
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-09 17:53:32 +0100

    added page body

--- pages/results/comments.tsx
+++ pages/results/comments.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import type { NextPage } from "next";
 
+import { Sidebar, Navbar } from "components/Layout";
+
 const Comments: NextPage = () => {
     return (
         <>
@@ -8,6 +10,14 @@ const Comments: NextPage = () => {
                 <title>Comments | CBT | Grand Regal School</title>
                 <meta name="description" content="Comments | GRS CBT" />
             </Head>
+            <section className="flex items-center justify-start w-screen h-screen divide-y-[1.5px] divide-gray-200">
+                <Sidebar />
+                <main className="flex flex-col grow items-center justify-center divide-x-[1.5px] divide-gray-200 h-full">
+                    <Navbar />
+                    <section className="flex flex-col gap-3 items-center justify-start w-full py-10 px-6 grow bg-gray-50/80 overflow-y-auto">
+                    </section>
+                </main>
+            </section>
         </>
     );
 }
