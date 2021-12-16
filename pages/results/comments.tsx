842f5f77ea6f25caf2dd13423708dffd74df7aa6
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-16 13:16:21 +0100

    completed body

--- pages/results/comments.tsx
+++ pages/results/comments.tsx
@@ -6,10 +6,14 @@ import { useState, useEffect } from "react";
 import Select from "components/Select";
 import { Sidebar, Navbar } from "components/Layout";
 
-import type { RouteData } from "types";
-import type { ClassesGETData } from "types/api/classes";
+import type { StudentCommentGETData } from "types/api/students";
+import type { ClientResponse, RouteData, StudentRecord } from "types";
+import type { ClassesGETData, ClassStudentsGETData } from "types/api/classes";
 
 const Comments: NextPage = () => {
+    const [students, setStudents] = useState<ClassStudentsGETData>([]);
+    const [comments, setComments] = useState<NonNullable<StudentCommentGETData>['comments']>();
+    const [selectedStudent, setSelectedStudent] = useState({ _id: "", name: "Select student" });
     const [selectedClass, setSelectedClass] = useState({ _id: "", name: "Loading classes..." });
     const { data: classes, error } = useSWR<RouteData<ClassesGETData>>("/api/classes?select=name", url => fetch(url).then(res => res.json()));
 
@@ -19,6 +23,36 @@ const Comments: NextPage = () => {
             name: (error !== undefined && classes === undefined) ? "Error Loading Classes" : (classes === undefined ? "Loading classes..." : "Select class")
         });
     }, [classes, error]);
+
+    async function getStudents(g: typeof selectedClass) {
+        setSelectedClass(g);
+
+        if (selectedClass._id !== "") {
+            try {
+                const res = await fetch(`/api/classes/${selectedClass._id}/students`);
+                const result = await res.json() as ClientResponse<ClassStudentsGETData>;
+
+                if (result.success === true) setStudents(result.data);
+                else throw new Error(result.error);
+            } catch (error: any) {
+                console.error({ error });
+            }
+        }
+    }
+
+    async function getComments() {
+        if (selectedStudent._id !== "") {
+            try {
+                const res = await fetch(`/api/students/${selectedStudent._id}/comments`);
+                const result = await res.json() as ClientResponse<StudentCommentGETData>;
+
+                if (result.success === true) setComments(result.data?.comments);
+                else throw new Error(result.error);
+            } catch (error: any) {
+                console.error({ error });
+            }
+        }
+    }
 
     return (
         <>
@@ -43,12 +77,61 @@ const Comments: NextPage = () => {
                                     buttonBorderColor: "focus-visible:border-indigo-500",
                                     buttonOffsetFocusColor: "focus-visible:ring-offset-indigo-500"
                                 }}
-                                handleChange={setSelectedClass}
+                                handleChange={getStudents}
                             />
-                            <button className="px-4 py-3 rounded-md shadow-md bg-gray-500 hover:bg-gray-600 text-white text-xs mb-3 min-w-max">
+                            <Select
+                                label="Student"
+                                options={students.map(({ _id, name: { full } }) => ({ _id, name: full }))}
+                                selected={selectedStudent}
+                                colorPallette={{
+                                    activeCheckIconColor: "stroke-indigo-600",
+                                    inactiveCheckIconColor: "stroke-indigo-800",
+                                    activeOptionColor: "text-indigo-900 bg-indigo-100",
+                                    buttonBorderColor: "focus-visible:border-indigo-500",
+                                    buttonOffsetFocusColor: "focus-visible:ring-offset-indigo-500"
+                                }}
+                                handleChange={setSelectedStudent}
+                            />
+                            <button
+                                onClick={getComments}
+                                className="px-4 py-3 rounded-md shadow-md bg-gray-500 hover:bg-gray-600 text-white text-xs mb-3 min-w-max"
+                            >
                                 Load Comments
                             </button>
                         </div>
+                        {comments !== undefined && (
+                            <form className="flex flex-col gap-7 items-center justify-start w-full py-10 px-3 grow">
+                                <h4 className="text-2xl font-extrabold uppercase tracking-wider text-gray-800">
+                                    {selectedStudent.name}
+                                </h4>
+                                <div className="flex flex-col gap-3 items-start justify-center w-full">
+                                    <h5 className="font-medium tracking-wide text-gray-700">
+                                        Head Teacher&apos;s Comment
+                                    </h5>
+                                    <textarea
+                                        value={comments.head}
+                                        onChange={e => setComments({
+                                            head: e.target.value ?? '',
+                                            class: comments.class ?? ''
+                                        })}
+                                        className="border-2 border-gray-600 rounded-lg p-3 w-full"
+                                    />
+                                </div>
+                                <div className="flex flex-col gap-3 items-start justify-center w-full">
+                                    <h5 className="font-medium tracking-wide text-gray-700">
+                                        Class Teacher&apos;s Comment
+                                    </h5>
+                                    <textarea
+                                        value={comments.class}
+                                        onChange={e => setComments({
+                                            head: comments.head ?? '',
+                                            class: e.target.value ?? ''
+                                        })}
+                                        className="border-2 border-gray-600 rounded-lg p-3 w-full"
+                                    />
+                                </div>
+                            </form>
+                        )}
                     </section>
                 </main>
             </section>
