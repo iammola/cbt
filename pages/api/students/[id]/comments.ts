02117b5c728a2b0f2ef1de8ab7bae3023d900eaf
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-16 18:40:02 +0100

    called `updateComments`function

--- pages/api/students/[id]/comments.ts
+++ pages/api/students/[id]/comments.ts
@@ -28,8 +28,6 @@ async function getComments(student: any): Promise<ServerResponse<StudentCommentG
     return [success, status, message];
 }
 
-export default async function handler({ method, query }: NextApiRequest, res: NextApiResponse) {
-    let [success, status, message]: ServerResponse<StudentCommentGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
 async function updateComments(student: any, comments: string): Promise<ServerResponse<StudentCommentPOSTData>> {
     await connect();
     let [success, status, message]: ServerResponse<StudentCommentPOSTData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
@@ -53,12 +51,14 @@ async function updateComments(student: any, comments: string): Promise<ServerRes
     return [success, status, message];
 }
 
+export default async function handler({ body, method, query }: NextApiRequest, res: NextApiResponse) {
+    let [success, status, message]: ServerResponse<StudentCommentGETData | StudentCommentPOSTData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
     const allowedMethods = ["GET", "POST"];
 
     if (allowedMethods.includes(method ?? '') === false) {
         res.setHeader("Allow", allowedMethods);
         [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
-    } else [success, status, message] = await (method === "POST" ? [success, status, message] : getComments(query.id));
+    } else[success, status, message] = await (method === "POST" ? updateComments(query.id, JSON.parse(body).comment) : getComments(query.id));
 
     if (typeof message !== "object") message = { message, error: message };
 
