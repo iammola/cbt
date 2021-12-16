2e91b05b4f77447c2d89c3446ef584c2bfd9ef7b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-16 13:15:56 +0100

    added GET function

--- pages/api/students/[id]/comments.ts
+++ pages/api/students/[id]/comments.ts
@@ -1,8 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { ResultModel } from "db/models";
+
 import type { ServerResponse } from "types";
 import type { StudentCommentGETData } from "types/api/students";
+
+async function getComments(student: any): Promise<ServerResponse<StudentCommentGETData>> {
+    await connect();
+    let [success, status, message]: ServerResponse<StudentCommentGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const data = await ResultModel.findOne({ student }, 'comments').lean();
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data,
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return[success, status, message];
+}
 
 export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: ServerResponse<StudentCommentGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
