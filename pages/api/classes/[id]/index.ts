679dc4a8b3dd35d1e6a00dff061b88b5fbb03dbc
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-17 18:12:14 +0100

    called `getClass` function

--- pages/api/classes/[id]/index.ts
+++ pages/api/classes/[id]/index.ts
@@ -28,14 +28,14 @@ async function getClass(id: any, select: any): Promise<ServerResponse<ClassGETDa
     return [success, status, message];
 }
 
-export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
+export default async function handler({ method, query }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: ServerResponse<ClassGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
     const allowedMethods = "GET";
 
     if (allowedMethods !== method) {
         res.setHeader("Allow", allowedMethods);
         [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
-    }
+    } else [success, status, message] = await getClass(query.id, query.select);
 
     if (typeof message !== "object") message = { message, error: message };
 
