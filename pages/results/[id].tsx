9e0be0610994051cdf2214cb31504efe48d6ad12
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-17 16:33:55 +0100

    added Head element

--- pages/results/[id].tsx
+++ pages/results/[id].tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import type { NextPage } from "next";
 
 const Result: NextPage = () => {
     return (
-        <></>
+        <section className="flex items-center justify-center w-screen min-h-screen bg-gray-200 p-10 xl:p-20 2xl:p-24">
+            <Head>
+                <title>Student • Results | CBT | Grand Regal School</title>
+                <meta name="description" content="Student • Results | GRS CBT" />
+            </Head>
+        </section>
     );
 }
 
