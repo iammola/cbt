4fc02348e625e3c9bb0694c240368f26def1f21b
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-17 18:09:58 +0100

    added `getClass` function

--- pages/api/classes/[id]/index.ts
+++ pages/api/classes/[id]/index.ts
@@ -1,10 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { ClassModel } from "db/models";
+
 import type { ServerResponse } from "types";
+import type { ClassGETData } from "types/api/classes";
+
+async function getClass(id: any, select: any): Promise<ServerResponse<ClassGETData>> {
+    await connect();
+    let [success, status, message]: ServerResponse<ClassGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const data = await ClassModel.findById(id, select).lean();
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data,
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
-    let [success, status, message]: ServerResponse<{}> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+    let [success, status, message]: ServerResponse<ClassGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
     const allowedMethods = "GET";
 
     if (allowedMethods !== method) {
--- types/api/classes.d.ts
+++ types/api/classes.d.ts
@@ -3,6 +3,8 @@ import type { ClassRecord, ClassResultTemplate, RecordId, ServerResponse, Studen
 export type ClassesGETData = ClassRecord[];
 export type ClassesPOSTData = ClassRecord;
 
+export type ClassGETData = ClassRecord | null;
+
 export type ClassSubjectPOSTData = Omit<SubjectRecord, '_id' | 'teachers'>;
 export type ClassSubjectGETData = Omit<SubjectsRecord, '_id' | 'class'> | null;
 
