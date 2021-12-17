a7abb0eb3d161ff840d1188370fb87f270214ff5
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-17 22:30:30 +0100

    called `getStudentSubjects` function

--- pages/api/students/[id]/subjects.ts
+++ pages/api/students/[id]/subjects.ts
@@ -48,14 +48,14 @@ async function getStudentSubjects(student: any): Promise<ServerResponse<StudentS
     return [success, status, message];
 }
 
-export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
+export default async function handler({ method, query }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: ServerResponse<StudentSubjectsGETData> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
     const allowedMethods = "GET";
 
     if (allowedMethods !== method) {
         res.setHeader("Allow", allowedMethods);
         [status, message] = [StatusCodes.METHOD_NOT_ALLOWED, ReasonPhrases.METHOD_NOT_ALLOWED];
-    }
+    } else [success, status, message] = await getStudentSubjects(query.id);
 
     if (typeof message !== "object") message = { message, error: message };
 
