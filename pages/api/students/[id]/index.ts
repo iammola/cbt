d13a13dbb7bd5b6df9e9902922a3513747761ebb
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-17 17:00:47 +0100

    added `getStudent` function

--- pages/api/students/[id]/index.ts
+++ pages/api/students/[id]/index.ts
@@ -1,7 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
+import { connect } from "db";
+import { StudentModel } from "db/models";
+
 import type { ServerResponse } from "types";
+
+async function getStudent(_id: any, select: any): Promise<ServerResponse<{}>> {
+    await connect();
+    let [success, status, message]: ServerResponse<{}> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
+
+    try {
+        const data = await StudentModel.findById(_id, select).lean();
+
+        [success, status, message] = [true, StatusCodes.OK, {
+            data,
+            message: ReasonPhrases.OK
+        }];
+    } catch (error: any) {
+        [status, message] = [StatusCodes.BAD_REQUEST, {
+            error: error.message,
+            message: ReasonPhrases.BAD_REQUEST
+        }];
+    }
+
+    return [success, status, message];
+}
 
 export default async function handler({ method }: NextApiRequest, res: NextApiResponse) {
     let [success, status, message]: ServerResponse<{}> = [false, StatusCodes.INTERNAL_SERVER_ERROR, ReasonPhrases.INTERNAL_SERVER_ERROR];
