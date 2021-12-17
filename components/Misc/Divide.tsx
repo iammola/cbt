e285144b9a6e2ba80354b763f2e3e8a087d4e019
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-17 16:40:21 +0100

    added className prop for `hr`

--- components/Misc/Divide.tsx
+++ components/Misc/Divide.tsx
@@ -2,13 +2,13 @@ import { FunctionComponent } from "react";
 
 import type { DivideProps } from "types";
 
-const Divide: FunctionComponent<DivideProps> = ({ className }) => {
+const Divide: FunctionComponent<DivideProps> = ({ className, HRclassName }) => {
     return (
         <div
             aria-hidden="true"
             className={className}
         >
-            <hr />
+            <hr className={HRclassName} />
         </div>
     );
 }
