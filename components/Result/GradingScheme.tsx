77375ed56cbba0344a8da6a35967f603de2f829c
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-18 16:56:08 +0100

    added `className` prop

--- components/Result/GradingScheme.tsx
+++ components/Result/GradingScheme.tsx
@@ -4,55 +4,59 @@ import { classNames } from "utils";
 
 import type { ClassRecord } from "types";
 
-const GradingScheme: FunctionComponent<GradingSchemeProps> = ({ scheme }) => {
+const GradingScheme: FunctionComponent<GradingSchemeProps> = ({ scheme, className }) => {
     return (
-        <table className="border border-gray-400 bg-white w-full">
-            <thead className="text-gray-700 divide-y divide-gray-400 font-medium">
-                <tr>
-                    <th
-                        scope="col"
-                        colSpan={3}
-                        className="text-xs text-center py-1.5"
-                    >
-                        Grading scheme
-                    </th>
-                </tr>
-                <tr className="text-xs bg-gray-100 divide-x divide-gray-400">
-                    {["Marks", "Grade", "Description"].map(col => (
+        <div className={className}>
+            <table className="border border-gray-400 bg-white w-full">
+                <thead className="text-gray-700 divide-y divide-gray-400 font-medium">
+                    <tr>
                         <th
-                            key={col}
                             scope="col"
-                            className="text-center w-1/3 py-2"
+                            colSpan={3}
+                            className="text-xs text-center py-1.5"
                         >
-                            {col}
+                            Grading scheme
                         </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-400 border-t border-gray-400 text-gray-600">
-                {scheme.sort((a, b) => b.limit - a.limit).map((item, index) => (
-                    <tr
-                        key={item.grade}
-                        className={classNames("text-xs text-center text-gray-800 font-medium divide-x divide-gray-400", {
-                            "bg-gray-100": index % 2 === 1
-                        })}
-                    >
-                        <td className="p-4">
-                            {item.limit}
-                        </td>
-                        <td className="p-4">
-                            {item.grade}
-                        </td>
-                        <td className="p-4">
-                            {item.description}
-                        </td>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                    <tr className="text-xs bg-gray-100 divide-x divide-gray-400">
+                        {["Marks", "Grade", "Description"].map(col => (
+                            <th
+                                key={col}
+                                scope="col"
+                                className="text-center w-1/3 py-2"
+                            >
+                                {col}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-400 border-t border-gray-400 text-gray-600">
+                    {scheme.sort((a, b) => b.limit - a.limit).map((item, index) => (
+                        <tr
+                            key={item.grade}
+                            className={classNames("text-xs text-center text-gray-800 font-medium divide-x divide-gray-400", {
+                                "bg-gray-100": index % 2 === 1
+                            })}
+                        >
+                            <td className="p-4">
+                                {item.limit}
+                            </td>
+                            <td className="p-4">
+                                {item.grade}
+                            </td>
+                            <td className="p-4">
+                                {item.description}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
-type GradingSchemeProps = Pick<ClassRecord["resultTemplate"][number]['terms'][number], 'scheme'>;
+interface GradingSchemeProps extends Pick<ClassRecord["resultTemplate"][number]['terms'][number], 'scheme'> {
+    className: string;
+}
 
 export default GradingScheme;
--- components/Result/ResultFields.tsx
+++ components/Result/ResultFields.tsx
@@ -4,53 +4,57 @@ import { classNames } from "utils";
 
 import type { ClassRecord } from "types";
 
-const ResultFields: FunctionComponent<ResultFieldsProps> = ({ fields }) => {
+const ResultFields: FunctionComponent<ResultFieldsProps> = ({ fields, className }) => {
     return (
-        <table className="border border-gray-400 bg-white w-full">
-            <thead className="text-gray-700 divide-y divide-gray-400 font-medium">
-                <tr>
-                    <th
-                        scope="col"
-                        colSpan={2}
-                        className="text-xs text-center py-1.5"
-                    >
-                        Result fields
-                    </th>
-                </tr>
-                <tr className="text-xs bg-gray-100 divide-x divide-gray-400">
-                    {["Name (Alias)", "Marks"].map(col => (
+        <div className={className}>
+            <table className="border border-gray-400 bg-white w-full">
+                <thead className="text-gray-700 divide-y divide-gray-400 font-medium">
+                    <tr>
                         <th
-                            key={col}
                             scope="col"
-                            className="text-center w-1/3 py-2"
+                            colSpan={2}
+                            className="text-xs text-center py-1.5"
                         >
-                            {col}
+                            Result fields
                         </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-400 border-t border-gray-400 text-gray-600">
-                {fields.map((item, index) => (
-                    <tr
-                        key={item.name}
-                        className={classNames("text-xs text-center text-gray-800 font-medium divide-x divide-gray-400", {
-                            "bg-gray-100": index % 2 === 1
-                        })}
-                    >
-                        <td className="p-4">
-                            {item.name} ({item.alias})
-                        </td>
-                        <td className="p-4">
-                            {item.max}
-                        </td>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                    <tr className="text-xs bg-gray-100 divide-x divide-gray-400">
+                        {["Name (Alias)", "Marks"].map(col => (
+                            <th
+                                key={col}
+                                scope="col"
+                                className="text-center w-1/3 py-2"
+                            >
+                                {col}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-400 border-t border-gray-400 text-gray-600">
+                    {fields.map((item, index) => (
+                        <tr
+                            key={item.name}
+                            className={classNames("text-xs text-center text-gray-800 font-medium divide-x divide-gray-400", {
+                                "bg-gray-100": index % 2 === 1
+                            })}
+                        >
+                            <td className="p-4">
+                                {item.name} ({item.alias})
+                            </td>
+                            <td className="p-4">
+                                {item.max}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
-type ResultFieldsProps = Pick<ClassRecord["resultTemplate"][number]['terms'][number], 'fields'>;
+interface ResultFieldsProps extends Pick<ClassRecord["resultTemplate"][number]['terms'][number], 'fields'> {
+    className: string;
+};
 
 
 export default ResultFields;
