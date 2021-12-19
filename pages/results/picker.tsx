1a473f4e5ba5461a923f9d6885a490ef51283555
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-19 09:02:51 +0100

    added page headings

--- pages/results/picker.tsx
+++ pages/results/picker.tsx
@@ -8,6 +8,15 @@ const ResultsPicker: NextPage = () => {
                 <title>Results Picker | Portal | Grand Regal School</title>
                 <meta name="description" content="Results | GRS Portal" />
             </Head>
+            <h3 className="text-5xl font-bold tracking-wide text-gray-800">
+                Load results for all students in a class
+            </h3>
+            <span className="text-gray-600 tracking-widest font-medium">
+                or
+            </span>
+            <h3 className="text-5xl font-bold tracking-wide text-gray-800">
+                Load for a single student
+            </h3>
         </section>
     );
 }
