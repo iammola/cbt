2f3880331e97dfd90d201377294d79cda2bc6e67
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-19 14:11:59 +0100

    hid advanced links

--- pages/results/picker.tsx
+++ pages/results/picker.tsx
@@ -9,6 +9,7 @@ import type { ClientResponse, RouteData } from "types";
 import type { ClassesGETData, ClassStudentsGETData } from "types/api/classes";
 
 const ResultsPicker: NextPage = () => {
+    const [advanced, setAdvanced] = useState(false);
     const [selectedStudent, setSelectedStudent] = useState({ _id: "", name: "Select student" });
     const [students, setStudents] = useState<{ class: any; students: ClassStudentsGETData; }[]>([]);
     const { data: classes } = useSWR<RouteData<ClassesGETData>>(`/api/classes/?select=name`, url => fetch(url ?? '').then(res => res.json()));
@@ -45,36 +46,40 @@ const ResultsPicker: NextPage = () => {
                 <title>Results Picker | Portal | Grand Regal School</title>
                 <meta name="description" content="Results | GRS Portal" />
             </Head>
-            <h3 className="text-5xl font-bold tracking-wide text-gray-800">
-                Load results for all students in a class
-            </h3>
-            <div className="flex flex-wrap gap-4 items-center justify-center w-full px-10">
-                {classes?.data.map(item => {
-                    const data = students.find(element => element.class === item._id)?.students ?? [];
+            {advanced === true && (
+                <>
+                    <h3 className="text-5xl font-bold tracking-wide text-gray-800">
+                        Load results for all students in a class
+                    </h3>
+                    <div className="flex flex-wrap gap-4 items-center justify-center w-full px-10">
+                        {classes?.data.map(item => {
+                            const data = students.find(element => element.class === item._id)?.students ?? [];
 
-                    return data?.length > 0 && (
-                        <div
-                            key={item.name}
-                            onClick={() => loadItems(item._id)}
-                            className="flex gap-6 items-center justify-center px-4 py-3 h-16 rounded-full shadow select-none cursor-pointer"
-                        >
-                            <span className="text-sm font-medium text-gray-700">
-                                {item.name}
-                            </span>
-                            <span className="text-xs text-gray-400">
-                                {data.length} student{data.length > 1 && "s"}
-                            </span>
-                        </div>
-                    );
-                })}
-            </div>
-            <span className="text-gray-600 tracking-widest font-medium">
-                or
-            </span>
-            <h3 className="text-5xl font-bold tracking-wide text-gray-800">
-                Load for a single student
+                            return data?.length > 0 && (
+                                <div
+                                    key={item.name}
+                                    onClick={() => loadItems(item._id)}
+                                    className="flex gap-6 items-center justify-center px-4 py-3 h-16 rounded-full shadow select-none cursor-pointer"
+                                >
+                                    <span className="text-sm font-medium text-gray-700">
+                                        {item.name}
+                                    </span>
+                                    <span className="text-xs text-gray-400">
+                                        {data.length} student{data.length > 1 && "s"}
+                                    </span>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <span className="text-gray-600 tracking-widest font-medium">
+                        or
+                    </span>
+                </>
+            )}
+            <h3 className="text-5xl font-bold tracking-wider text-gray-800">
+                Select student
             </h3>
-            <div className="flex gap-8 items-center justify-center w-full px-20">
+            <div className="flex flex-col gap-10 items-center justify-center w-10 px-20 pt-8">
                 <Select
                     selected={selectedStudent}
                     handleChange={setSelectedStudent}
@@ -86,7 +91,7 @@ const ResultsPicker: NextPage = () => {
                         const { _id } = selectedStudent;
                         _id !== "" && openTab(_id);
                     }}
-                    className="px-10 py-3 bg-slate-500 hover:bg-slate-600 text-white text-xs font-bold shadow rounded-full uppercase tracking-widest"
+                    className="px-10 py-3 bg-slate-500 hover:bg-slate-600 text-white text-xs font-bold shadow rounded-full uppercase tracking-widest min-w-max"
                 >
                     Load
                 </button>
