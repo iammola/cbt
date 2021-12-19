a770404cf6857087307cffaf18315e75639b0c4d
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-19 09:23:53 +0100

    fetched class's students

--- pages/results/picker.tsx
+++ pages/results/picker.tsx
@@ -1,12 +1,31 @@
 import useSWR from "swr";
 import Head from "next/head";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
-import type { RouteData } from "types";
-import type { ClassesGETData } from "types/api/classes";
+import type { ClientResponse, RouteData } from "types";
+import type { ClassesGETData, ClassStudentsGETData } from "types/api/classes";
 
 const ResultsPicker: NextPage = () => {
+    const [students, setStudents] = useState<{ class: any; students: ClassStudentsGETData; }[]>([]);
     const { data: classes } = useSWR<RouteData<ClassesGETData>>(`/api/classes/?select=name`, url => fetch(url ?? '').then(res => res.json()));
+
+    useEffect(() => {
+        async function getStudents(classes: ClassesGETData) {
+            await Promise.all(classes.map(async ({ _id }) => {
+                const res = await fetch(`/api/classes/${_id}/students/`);
+                const result = await res.json() as ClientResponse<ClassStudentsGETData>;
+
+                if (result.success === true) setStudents(students => [...students.filter(j => j.class !== _id), {
+                    class: _id,
+                    students: result.data
+                }]);
+            }));
+            console.log("Loaded Students");
+        }
+
+        if (classes !== undefined) getStudents(classes.data);
+    }, [classes]);
 
     return (
         <section className="flex flex-col gap-7 items-center justify-center w-screen h-screen">
