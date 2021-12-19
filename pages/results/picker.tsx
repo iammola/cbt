0fec0872049e4e58949c554ecba689aef55ff2f9
Ademola Adedeji <a.mola.dev@gmail.com>
2021-12-19 08:52:24 +0100

    added Head element

--- pages/results/picker.tsx
+++ pages/results/picker.tsx
@@ -1,8 +1,14 @@
+import Head from "next/head";
 import type { NextPage } from "next";
 
 const ResultsPicker: NextPage = () => {
     return (
-        <></>
+        <section className="flex flex-col gap-7 items-center justify-center w-screen h-screen">
+            <Head>
+                <title>Results Picker | Portal | Grand Regal School</title>
+                <meta name="description" content="Results | GRS Portal" />
+            </Head>
+        </section>
     );
 }
 
