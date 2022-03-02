181d61b754b0a1ea278b048be05f3cb586ec4886
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-02 15:11:31 +0100

    feat(api): return 401 Response in middleware :sparkles:

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -2,5 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware({ cookies, nextUrl: { pathname } }: NextRequest) {
     if (cookies.account !== undefined || pathname.endsWith('/login/') || pathname.endsWith('/ping/')) return NextResponse.next();
-    return NextResponse.error();
+    return new Response(null, {
+        status: 401,
+        statusText: "Unauthorized"
+    });
 }
