281cc1aa2b2552af25ac15d4c094706af05524de
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-02 20:55:14 +0100

    dev(api): add POST handler :construction_worker:

--- pages/api/sessions/[id]/terms.ts
+++ pages/api/sessions/[id]/terms.ts
@@ -1,13 +1,61 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
-import type { ServerResponse } from "types";
+import { connect } from "db";
+import { SessionModel } from "db/models";
+
+import type { TermGETData } from "types/api/sessions";
+import type { ServerResponse, TermRecord } from "types";
+
+async function createTerm(
+  id: string,
+  term: TermRecord
+): Promise<ServerResponse<TermGETData>> {
+  await connect();
+  let [success, status, message]: ServerResponse<TermGETData> = [
+    false,
+    StatusCodes.INTERNAL_SERVER_ERROR,
+    ReasonPhrases.INTERNAL_SERVER_ERROR,
+  ];
+
+  try {
+    const data = await SessionModel.findByIdAndUpdate(
+      id,
+      { $push: { terms: term } },
+      {
+        runValidators: true,
+        returnDocument: "after",
+      }
+    ).lean();
+
+    if (data === null) throw new Error("Invalid Session ID");
+
+    [success, status, message] = [
+      true,
+      StatusCodes.OK,
+      {
+        data: { _id: data.terms.slice(-1)[0]._id },
+        message: ReasonPhrases.OK,
+      },
+    ];
+  } catch (error: any) {
+    [status, message] = [
+      StatusCodes.BAD_REQUEST,
+      {
+        error: error.message,
+        message: ReasonPhrases.BAD_REQUEST,
+      },
+    ];
+  }
+
+  return [success, status, message];
+}
 
 export default async function handler(
-  { method }: NextApiRequest,
+  { method, query, body }: NextApiRequest,
   res: NextApiResponse
 ) {
-  let [success, status, message]: ServerResponse<{}> = [
+  let [success, status, message]: ServerResponse<TermGETData> = [
     false,
     StatusCodes.INTERNAL_SERVER_ERROR,
     ReasonPhrases.INTERNAL_SERVER_ERROR,
@@ -20,7 +68,11 @@ export default async function handler(
       StatusCodes.METHOD_NOT_ALLOWED,
       ReasonPhrases.METHOD_NOT_ALLOWED,
     ];
-  }
+  } else
+    [success, status, message] = await createTerm(
+      query.id as string,
+      JSON.parse(body)
+    );
 
   if (typeof message !== "object") message = { message, error: message };
 
