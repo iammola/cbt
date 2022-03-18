e24c4684b0773b8217d39545007f88859e6931a4
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-18 10:53:56 +0100

    feat(TranscriptInfo): add content 👍

--- components/Result/TranscriptInfo.tsx
+++ components/Result/TranscriptInfo.tsx
@@ -1,7 +1,55 @@
 import { FunctionComponent } from "react";
+import { format, formatDuration, intervalToDuration } from "date-fns";
 
-const TranscriptInfo: FunctionComponent<TranscriptInfoProps> = () => {
-  return <></>;
+const TranscriptInfo: FunctionComponent<TranscriptInfoProps> = ({
+  birthday,
+  gender,
+  name,
+}) => {
+  return (
+    <div className="grid w-full grid-flow-col grid-cols-3 grid-rows-1 gap-y-2.5 gap-x-40 px-5">
+      <div className="flex items-center justify-start gap-1.5">
+        <span className="min-w-max text-xs font-semibold tracking-wide text-gray-700">
+          Full Name
+        </span>{" "}
+        <span className="min-w-max text-sm font-bold uppercase tracking-wide text-gray-800">
+          {name}
+        </span>
+      </div>
+      <div className="flex items-center justify-start gap-1.5">
+        <span className="min-w-max text-xs font-semibold tracking-wide text-gray-700">
+          Age:
+        </span>{" "}
+        <span className="min-w-max text-sm font-bold tracking-wide text-gray-800">
+          {birthday !== undefined &&
+            formatDuration(
+              intervalToDuration({
+                start: new Date(birthday),
+                end: new Date(),
+              }),
+              { format: ["years"] }
+            )}{" "}
+          old
+        </span>
+      </div>
+      <div className="flex items-center justify-start gap-1.5">
+        <span className="min-w-max text-xs font-semibold tracking-wide text-gray-700">
+          Gender:
+        </span>{" "}
+        <span className="min-w-max text-sm font-medium tracking-wide text-gray-800">
+          {gender && gender === "M" ? "Male" : "Female"}
+        </span>
+      </div>
+      <div className="flex items-center justify-start gap-1.5">
+        <span className="min-w-max text-xs font-semibold tracking-wide text-gray-700">
+          Printed On:
+        </span>{" "}
+        <span className="min-w-max text-sm font-medium tracking-wide text-gray-800">
+          {format(new Date(), "dd-MM-yyyy")}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 type TranscriptInfoProps = {
