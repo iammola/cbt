f795a5d1ca1b7cc000a4deba2fc549cd014c3c58
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-18 12:53:16 +0100

    dev(TranscriptFooter): add content elements ⌨️

--- components/Result/TranscriptFooter.tsx
+++ components/Result/TranscriptFooter.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
+import { format } from "date-fns";
 import { FunctionComponent } from "react";
 
 const TranscriptFooter: FunctionComponent = () => {
-  return <></>;
+  return (
+    <div className="w-full space-y-3">
+      <div className="flex items-center justify-start gap-x-6">
+        <span className="text-sm font-medium tracking-wide text-slate-600">
+          Principal Remark:
+        </span>
+        <div className="tracking-wide text-slate-600">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,
+          commodi!
+        </div>
+      </div>
+      <div className="flex items-center justify-start gap-x-6">
+        <span className="text-sm font-medium tracking-wide text-slate-600">
+          Principal Signature:
+        </span>
+        <div className="relative h-16 w-[166px] self-end">
+          <Image
+            priority
+            layout="fill"
+            alt="VP Signature"
+            objectFit="contain"
+            src="/Signature VP.png"
+            objectPosition="center"
+            className="brightness-50"
+          />
+        </div>
+      </div>
+      <div className="flex items-center justify-start gap-x-6">
+        <span className="text-sm font-medium tracking-wide text-slate-600">
+          Date:
+        </span>
+        <div className="border-b-2 border-dotted border-slate-500 px-6 py-2 text-xl tracking-wide text-slate-600">
+          {format(new Date(), "dd-MM-yyyy")}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TranscriptFooter;
