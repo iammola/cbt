017af2875a48c32ad0ac748fb1d73bb7ee6f097b
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-18 10:44:04 +0100

    dev(TranscriptInfo): add props ⛏

--- components/Result/TranscriptInfo.tsx
+++ components/Result/TranscriptInfo.tsx
@@ -1,7 +1,13 @@
 import { FunctionComponent } from "react";
 
-const TranscriptInfo: FunctionComponent = () => {
+const TranscriptInfo: FunctionComponent<TranscriptInfoProps> = () => {
   return <></>;
+};
+
+type TranscriptInfoProps = {
+  name?: string;
+  birthday?: Date;
+  gender?: "M" | "F";
 };
 
 export default TranscriptInfo;
