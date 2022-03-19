ce9a25582da401631a8bb5e7e8eca039c62151ee
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-19 20:42:50 +0100

    fix(api): correct query operator spelling 🐛

--- pages/api/subjects/[id]/students.ts
+++ pages/api/subjects/[id]/students.ts
@@ -17,7 +17,7 @@ async function getStudents({ id, term }: any): Promise<ServerResponse<SubjectStu
 
   try {
     const students = await StudentModel.find(
-      { academic: { $elemMAtch: { term, subjects: id } } },
+      { academic: { $elemMatch: { term, subjects: id } } },
       "name.full name.initials"
     ).lean();
 
