156596cb941cceb763cc5142f03b6b2cf9c403a6
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-20 02:34:36 +0100

    dev(actions): hide actions in print 👍

--- components/Result/Actions.tsx
+++ components/Result/Actions.tsx
@@ -4,7 +4,7 @@ import { ChevronLeftIcon, PrinterIcon } from "@heroicons/react/solid";
 
 const Actions: FunctionComponent<{ pickerLink: string }> = ({ pickerLink }) => {
   return (
-    <div className="fixed bottom-10 flex items-center justify-between gap-x-6 rounded-full bg-slate-200 py-4 px-6 shadow">
+    <div className="fixed bottom-10 flex items-center justify-between gap-x-6 rounded-full bg-slate-200 py-4 px-6 shadow print:hidden">
       <Link href={pickerLink}>
         <a className="block">
           <div className="aspect-square h-[4.5rem] w-[4.5rem] cursor-pointer overflow-hidden rounded-full bg-white p-3.5 shadow shadow-slate-800/25 hover:bg-gray-50">
