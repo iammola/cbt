ad3d9a694477ee9563609d8215df0d461ac5e77c
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-20 02:37:47 +0100

    feat(transcript): add print width styles 💪

--- pages/results/[id]/transcript.tsx
+++ pages/results/[id]/transcript.tsx
@@ -36,7 +36,7 @@ const ResultTranscript: NextPage = () => {
   }, [classError, error, studentError]);
 
   return (
-    <section className="flex min-h-screen w-screen items-center justify-center bg-gray-200 py-16 print:bg-white print:p-0">
+    <section className="flex min-h-screen w-screen items-center justify-center bg-gray-200 py-16 print:w-full print:bg-white print:p-0">
       <Head>
         <title>{student?.data?.name.full ?? "Loading student's"} Transcript | Grand Regal School</title>
         <meta
@@ -44,7 +44,7 @@ const ResultTranscript: NextPage = () => {
           content="Student • Transcript | GRS CBT"
         />
       </Head>
-      <main className="flex aspect-[1/1.4142] w-[60rem] flex-col items-center justify-start rounded-lg bg-white p-12 shadow-xl shadow-gray-500/30 print:rounded-none print:px-8 print:py-5 print:shadow-none">
+      <main className="flex aspect-[1/1.4142] w-[60rem] flex-col items-center justify-start rounded-lg bg-white p-12 shadow-xl shadow-gray-500/30 print:aspect-auto print:min-h-screen print:rounded-none print:px-8 print:py-5 print:shadow-none">
         <Header />
         <Divide
           className="w-full py-7"
