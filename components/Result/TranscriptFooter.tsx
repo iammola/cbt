4a8ef841d343d6a704ae1415e1ce7910742d62a1
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-20 11:53:45 +0100

    feat(transcript-footer): add apostrophe to principal titles 🖊

--- components/Result/TranscriptFooter.tsx
+++ components/Result/TranscriptFooter.tsx
@@ -5,13 +5,13 @@ const TranscriptFooter: FunctionComponent = () => {
   return (
     <div className="w-full space-y-3">
       <div className="flex items-center justify-start gap-x-6">
-        <span className="text-sm font-medium tracking-wide text-slate-600">Principal Remark:</span>
+        <span className="text-sm font-medium tracking-wide text-slate-600">Principal&apos;s Remark:</span>
         <div className="tracking-wide text-slate-600">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, commodi!
         </div>
       </div>
       <div className="flex items-center justify-start gap-x-6">
-        <span className="text-sm font-medium tracking-wide text-slate-600">Principal Signature:</span>
+        <span className="text-sm font-medium tracking-wide text-slate-600">Principal&apos;s Signature:</span>
         <div className="relative h-16 w-[166px] self-end">
           <Image
             priority
