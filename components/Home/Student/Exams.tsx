bd45910209247ba990e6d88549e88344c162fe43
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-22 03:26:07 +0100

    feat(exams): create cards skeleton ✨

--- components/Home/Student/Exams.tsx
+++ components/Home/Student/Exams.tsx
@@ -7,26 +7,25 @@ const Exam: FunctionComponent = () => {
     <Section>
       <Title>Exams</Title>
       <Cards>
-        <Card>
-          <h5 className="text-2xl font-bold text-gray-700 line-clamp-2">Information and Communication Technology</h5>
-          <ul className="w-full list-inside list-disc space-y-1">
-            <li className="text-sm text-slate-700">
-              <span className="font-medium">50</span> questions
-            </li>
-            <li className="text-sm text-slate-700">
-              <span className="font-medium">60</span> minutes
-            </li>
-            <li className="text-sm font-medium text-slate-700">Wednesday, 23rd March 2022</li>
-          </ul>
-          <button
-            type="button"
-            className="w-full cursor-pointer rounded-full bg-blue-500 py-2 px-8 text-sm text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 focus-visible:ring-offset-white"
-          >
-            Start Exam
-          </button>
-        </Card>
+        {new Array(4).fill(0).map((_, i) => (
+          <Skeleton key={i} />
+        ))}
       </Cards>
     </Section>
+  );
+};
+
+const Skeleton: FunctionComponent = () => {
+  return (
+    <aside className="w-[300px] space-y-5 rounded-xl bg-white px-5 py-4 shadow">
+      <div className="h-4 w-full animate-pulse rounded-full bg-slate-300" />
+      <div className="w-full space-y-2">
+        <div className="h-2 w-2/5 animate-pulse rounded-full bg-slate-300" />
+        <div className="h-2 w-3/5 animate-pulse rounded-full bg-slate-300" />
+        <div className="h-2 w-4/5 animate-pulse rounded-full bg-slate-300" />
+      </div>
+      <div className="w-full animate-pulse rounded-full bg-slate-300 py-2" />
+    </aside>
   );
 };
 
