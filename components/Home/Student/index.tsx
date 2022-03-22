cfdaebe5740b629703c95d312dc4779ab99f1924
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-22 01:40:31 +0100

    feat(home): add title and sign out button 👏
    s

--- components/Home/Student/index.tsx
+++ components/Home/Student/index.tsx
@@ -6,16 +6,26 @@ const Home: FunctionComponent = () => {
   const router = useRouter();
   const [{ account }, , removeCookies] = useCookies(["account"]);
 
+  const signOut = () => router.push("/").then(() => removeCookies("account", { path: "/" }));
 
   return (
-    <section className="h-screen w-screen">
+    <section className="flex h-screen w-screen flex-col items-start justify-start">
+      <header className="flex w-full items-center justify-between border-b-2 border-slate-200 bg-white py-6 px-5">
+        <div className="space-y-2">
+          <h2 className="text-3xl font-bold tracking-wider text-slate-700">Dashboard 🚀</h2>
+          <div className="text-sm tracking-wide">
+            <span className="text-slate-500">Logged in as</span>{" "}
+            <span className="font-medium text-slate-700">{account.name?.full}</span>
           </div>
         </div>
         <button
           type="button"
+          onClick={signOut}
+          className="cursor-pointer rounded-full bg-blue-500 py-2 px-8 text-sm text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 focus-visible:ring-offset-white"
         >
+          Sign out
         </button>
-      </div>
+      </header>
     </section>
   );
 };
