190b45b578c9de2d88a43bb847cc386d3a894875
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-22 07:24:14 +0100

    feat(results): create cards skeleton 💪

--- components/Home/Student/Results.tsx
+++ components/Home/Student/Results.tsx
@@ -7,20 +7,24 @@ const Result: FunctionComponent = () => {
     <Section>
       <Title>Results</Title>
       <Cards>
-        <Card className="h-48 gap-y-3">
-          <h5 className="text-4xl font-bold tracking-wide text-gray-700">41</h5>
-          <h6 className="text-lg font-medium text-gray-600 line-clamp-2">Information and Communication Technology</h6>
-          <ul className="w-full list-inside list-disc space-y-1">
-            <li className="text-sm text-slate-500">
-              Spent <span className="font-medium text-slate-700">30</span> minutes
-            </li>
-            <li className="text-sm text-slate-500">
-              Answered <span className="font-medium text-slate-700">50</span> questions
-            </li>
-          </ul>
-        </Card>
+        {new Array(4).fill(0).map((_, i) => (
+          <Skeleton key={i} />
+        ))}
       </Cards>
     </Section>
+  );
+};
+
+const Skeleton: FunctionComponent = () => {
+  return (
+    <aside className="w-[300px] space-y-5 rounded-xl bg-white px-5 py-4 shadow">
+      <div className="h-4 w-full animate-pulse rounded-full bg-slate-300" />
+      <div className="h-3 w-full animate-pulse rounded-full bg-slate-300" />
+      <div className="w-full space-y-2">
+        <div className="h-2 w-2/5 animate-pulse rounded-full bg-slate-300" />
+        <div className="h-2 w-3/5 animate-pulse rounded-full bg-slate-300" />
+      </div>
+    </aside>
   );
 };
 
