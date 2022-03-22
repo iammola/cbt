ce9891b67dc0f843c821b8fca44fd329b17979af
Ademola Adedeji <a.mola.dev@gmail.com>
2022-03-22 07:46:06 +0100

    feat(results): fetch results data 👏

--- components/Home/Student/Results.tsx
+++ components/Home/Student/Results.tsx
@@ -1,15 +1,41 @@
+import useSWR from "swr";
 import { FunctionComponent } from "react";
+import { useCookies } from "react-cookie";
 
 import { Card, Cards, Section, Title } from "./Section";
 
+import type { RouteData } from "types";
+import type { StudentCBTResultsGETData } from "types/api";
+
 const Result: FunctionComponent = () => {
+  const [{ account }] = useCookies(["account"]);
+  const { data: { data } = {}, error } = useSWR<RouteData<StudentCBTResultsGETData>>(
+    `/api/students/${account._id}/cbt_results/`
+  );
+
   return (
     <Section>
       <Title>Results</Title>
       <Cards>
-        {new Array(4).fill(0).map((_, i) => (
-          <Skeleton key={i} />
+        {data?.map((item, idx) => (
+          <Card
+            key={idx}
+            className="h-48 gap-y-3"
+          >
+            <h5 className="text-4xl font-bold tracking-wide text-gray-700">{item.score}</h5>
+            <h6 className="text-lg font-medium text-gray-600 line-clamp-2">{item.subject}</h6>
+            <ul className="w-full list-inside list-disc space-y-1">
+              <li className="text-sm text-slate-500">
+                Spent <span className="font-medium text-slate-700">{item.time}</span> minute{item.time !== 1 && "s"}
+              </li>
+              <li className="text-sm text-slate-500">
+                Attempted <span className="font-medium text-slate-700">{item.attempts}</span> question
+                {item.attempts !== 1 && "s"}
+              </li>
+            </ul>
+          </Card>
         ))}
+        {!data && !error && new Array(4).fill(0).map((_, i) => <Skeleton key={i} />)}
       </Cards>
     </Section>
   );
