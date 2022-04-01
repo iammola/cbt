a7203afdc91b634f2647e2e070887d62c63b7893
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-01 23:11:48 +0100

    dev: add flex breakpoints 🖊

--- pages/results/create.tsx
+++ pages/results/create.tsx
@@ -263,7 +263,7 @@ const Results: NextPage = () => {
       <main className="h-full grow divide-x-[1.5px] divide-gray-200 overflow-x-auto">
         <Navbar />
         <section className="flex w-full grow flex-col items-center justify-start gap-3 overflow-y-auto bg-gray-50/80 py-10 px-6">
-          <div className="flex w-full items-end justify-center gap-4">
+          <div className="flex w-full flex-col items-center justify-start gap-x-4 sm:flex-row sm:items-end sm:justify-center">
             <Select
               label="Terms"
               options={terms?.data}
