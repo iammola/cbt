225abaa5e5f8822ad61e839772e414a0f764a616
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-04 02:22:07 +0100

    dev(check-incomplete): add router hook query 🪛

--- pages/results/check-incomplete/[type].tsx
+++ pages/results/check-incomplete/[type].tsx
@@ -1,22 +1,25 @@
 import Head from "next/head";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
 import { Sidebar } from "components/Layout";
 
 const CheckTypeIncompleteResults: NextPage = () => {
+  const router = useRouter();
+
   return (
     <section className="flex h-screen w-screen items-center justify-start divide-y-[1.5px] divide-gray-200">
       <Sidebar />
       <Head>
-        <title>Check `Type` Incomplete Results | CBT | Grand Regal School</title>
+        <title>Check {router.query.type} Incomplete Results | CBT | Grand Regal School</title>
         <meta
           name="description"
-          content="`Type` Incomplete Results | GRS CBT"
+          content={`${router.query.type} Incomplete Results | GRS CBT`}
         />
       </Head>
       <section className="flex h-screen w-screen grow flex-col items-center justify-start gap-7">
         <h3 className="text-center text-5xl font-bold tracking-wider text-gray-600">
-          <span className="block">Check `type` incomplete results</span>
+          <span className="block">Check {router.query.type} results</span>
         </h3>
       </section>
     </section>
