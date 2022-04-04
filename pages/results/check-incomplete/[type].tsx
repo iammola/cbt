30d2694fbfee2d3f340228bba3c8b0c571af093f
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-04 02:24:30 +0100

    feat(check-incomplete): add term select 💪

--- pages/results/check-incomplete/[type].tsx
+++ pages/results/check-incomplete/[type].tsx
@@ -1,11 +1,37 @@
+import useSWR from "swr";
 import Head from "next/head";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
+import Select from "components/Select";
 import { Sidebar } from "components/Layout";
+
+import type { RouteData } from "types";
+import type { AllTermsGetData } from "types/api";
 
 const CheckTypeIncompleteResults: NextPage = () => {
   const router = useRouter();
+
+  const { data: terms } = useSWR<RouteData<AllTermsGetData>>("/api/terms/all");
+
+  const [selectedTerm, setSelectedTerm] = useState({
+    _id: "",
+    name: "Loading terms...",
+  });
+
+  useEffect(() => {
+    if (!selectedTerm._id && terms?.data !== undefined) {
+      const term = terms.data.find((i) => i.current) as unknown as typeof selectedTerm;
+
+      setSelectedTerm(
+        term ?? {
+          _id: "",
+          name: "Select term",
+        }
+      );
+    }
+  }, [selectedTerm, terms]);
 
   return (
     <section className="flex h-screen w-screen items-center justify-start divide-y-[1.5px] divide-gray-200">
@@ -21,6 +47,14 @@ const CheckTypeIncompleteResults: NextPage = () => {
         <h3 className="text-center text-5xl font-bold tracking-wider text-gray-600">
           <span className="block">Check {router.query.type} results</span>
         </h3>
+        <div className="flex items-center justify-center gap-x-10">
+          <Select
+            label="Term"
+            options={terms?.data}
+            selected={selectedTerm}
+            handleChange={setSelectedTerm}
+          />
+        </div>
       </section>
     </section>
   );
