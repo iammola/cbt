827cef845af528d512006af78a5d971902f5912e
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-17 14:51:40 +0100

    feat(student): add basic elements 👍

--- pages/students/[id].tsx
+++ pages/students/[id].tsx
@@ -1,7 +1,17 @@
+import { Sidebar, Navbar } from "components/Layout";
+
 import type { NextPage } from "next";
 
 const Student: NextPage = () => {
-  return <></>;
+  return (
+    <section className="flex h-screen w-screen items-center justify-start divide-y-[1.5px] divide-gray-200">
+      <Sidebar />
+      <main className="flex h-full grow flex-col items-center justify-center divide-x-[1.5px] divide-gray-200">
+        <Navbar />
+        <section className="flex w-full grow flex-col items-start justify-start gap-7 overflow-y-auto bg-gray-50 py-7 px-6"></section>
+      </main>
+    </section>
+  );
 };
 
 export default Student;
