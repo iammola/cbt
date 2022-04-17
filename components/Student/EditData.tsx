b2f73c100f7bf001cf77d6aeb55be57ae555aadf
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-17 15:15:15 +0100

    feat(edit-data): add form, state and functions 👍

--- components/Student/EditData.tsx
+++ components/Student/EditData.tsx
@@ -1,11 +1,20 @@
-import { FunctionComponent } from "react";
+import { FormEvent, FunctionComponent, useState } from "react";
 
 import type { StudentGETData } from "types/api";
 
-export const EditData: FunctionComponent<Props> = () => {
-  return <></>;
+export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
+  const [data, setData] = useState(props);
+  const updateData = <T extends keyof Props>(key: T, value: Props[T]) => setData((old) => ({ ...old, [key]: value }));
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    onSubmit(data);
+  }
+
+  return <form onSubmit={handleSubmit}></form>;
 };
 
-type Props = Omit<StudentGETData, "academic">;
+type P = NonNullable<StudentGETData>;
+type Props = Omit<P, "academic"> & { onSubmit(props: Omit<P, "academic">): void };
 
 export default EditData;
