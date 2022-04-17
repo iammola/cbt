b52008af9eab5f8d6eb876a75f3353f9c137a674
Ademola Adedeji <a.mola.dev@gmail.com>
2022-04-17 23:50:08 +0100

    enh(student): set input widths 🎨

--- components/Student/EditData.tsx
+++ components/Student/EditData.tsx
@@ -23,7 +23,7 @@ export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
       onSubmit={handleSubmit}
       className="flex flex-wrap gap-x-5 gap-y-4"
     >
-      <div className="flex w-full flex-col gap-2.5">
+      <div className="flex w-full max-w-[30rem] flex-col gap-2.5">
         <label
           htmlFor="fullName"
           className="min-w-[20rem] text-sm font-semibold text-gray-600"
@@ -39,7 +39,7 @@ export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
           className="rounded-md border p-3 pl-5 transition-shadow focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
       </div>
-      <div className="flex w-full flex-col gap-2.5">
+      <div className="flex w-full max-w-[10rem] flex-col gap-2.5">
         <label
           htmlFor="initials"
           className="text-sm font-semibold text-gray-600"
@@ -57,7 +57,7 @@ export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
           className="rounded-md border p-3 pl-5 transition-shadow focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
       </div>
-      <div className="flex w-full flex-col gap-2.5">
+      <div className="flex w-full max-w-[20rem] flex-col gap-2.5">
         <label
           htmlFor="email"
           className="text-sm font-semibold text-gray-600"
@@ -73,7 +73,7 @@ export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
           className="rounded-md border p-3 pl-5 transition-shadow focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
       </div>
-      <div className="flex w-full flex-col gap-2.5">
+      <div className="flex w-full max-w-[20rem] flex-col gap-2.5">
         <label
           htmlFor="firstName"
           className="text-sm font-semibold text-gray-600"
@@ -89,7 +89,7 @@ export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
           className="rounded-md border p-3 pl-5 transition-shadow focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
       </div>
-      <div className="flex w-full flex-col gap-2.5">
+      <div className="flex w-full max-w-[20rem] flex-col gap-2.5">
         <label
           htmlFor="lastName"
           className="text-sm font-semibold text-gray-600"
@@ -105,20 +105,22 @@ export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
           className="rounded-md border p-3 pl-5 transition-shadow focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
       </div>
-      <Select
-        label="Gender"
-        colorPallette={{
-          activeCheckIconColor: "stroke-indigo-600",
-          inactiveCheckIconColor: "stroke-indigo-800",
-          activeOptionColor: "text-indigo-900 bg-indigo-100",
-          buttonBorderColor: "focus-visible:border-indigo-500",
-          buttonOffsetFocusColor: "focus-visible:ring-offset-indigo-500",
-        }}
-        options={genders}
-        handleChange={(e) => updateData("gender", e._id as "M" | "F")}
-        selected={genders.find((g) => g._id === data.gender) ?? genders[0]}
-      />
-      <div className="flex w-full flex-col gap-2.5">
+      <div className="max-w-[20rem]">
+        <Select
+          label="Gender"
+          colorPallette={{
+            activeCheckIconColor: "stroke-indigo-600",
+            inactiveCheckIconColor: "stroke-indigo-800",
+            activeOptionColor: "text-indigo-900 bg-indigo-100",
+            buttonBorderColor: "focus-visible:border-indigo-500",
+            buttonOffsetFocusColor: "focus-visible:ring-offset-indigo-500",
+          }}
+          options={genders}
+          handleChange={(e) => updateData("gender", e._id as "M" | "F")}
+          selected={genders.find((g) => g._id === data.gender) ?? genders[0]}
+        />
+      </div>
+      <div className="flex w-full max-w-[20rem] flex-col gap-2.5">
         <label
           htmlFor="birthday"
           className="text-sm font-semibold text-gray-600"
@@ -134,12 +136,14 @@ export const EditData: FunctionComponent<Props> = ({ onSubmit, ...props }) => {
           className="rounded-md border p-3 pl-5 transition-shadow focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
       </div>
-      <button
-        type="submit"
-        className="mt-3 flex items-center justify-center gap-4 rounded-md bg-indigo-400 py-2.5 px-3 text-white shadow-md transition-colors hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2  focus:ring-offset-white"
-      >
-        Save Changes
-      </button>
+      <div className="w-full">
+        <button
+          type="submit"
+          className="mt-3 flex items-center justify-center gap-4 rounded-md bg-indigo-400 py-2.5 px-3 text-white shadow-md transition-colors hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2  focus:ring-offset-white"
+        >
+          Save Changes
+        </button>
+      </div>
     </form>
   );
 };
