2d66c92a48f2369dc1c280430c4fcde326181082
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-06 19:39:46 +0100

    🎨 style: format files

--- pages/api/validate.ts
+++ pages/api/validate.ts
@@ -7,23 +7,27 @@ import type { ResultRecord } from "types";
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   await connect();
-  
+
   const data = [] as string[];
   const results = await ResultModel.find({}, "data");
 
-  await Promise.all(results.map(async result  => {
-    const checked = Object.values(result.data.reduce((acc, d) => {
-      return {
-        ...acc,
-        [d.subject.toString()]: d
-      }
-    }, {} as Record<string, ResultRecord["data"][number]>));
+  await Promise.all(
+    results.map(async (result) => {
+      const checked = Object.values(
+        result.data.reduce((acc, d) => {
+          return {
+            ...acc,
+            [d.subject.toString()]: d,
+          };
+        }, {} as Record<string, ResultRecord["data"][number]>)
+      );
 
-    if (checked.length !== result.data.length) {
-      await result.updateOne({ data: checked });
-      data.push(`Result ${result._id.toString()} - From ${result.data.length} To ${checked.length}`);
-    }
-  }));
+      if (checked.length !== result.data.length) {
+        await result.updateOne({ data: checked });
+        data.push(`Result ${result._id.toString()} - From ${result.data.length} To ${checked.length}`);
+      }
+    })
+  );
 
   res.send(data.join("\n"));
 }
