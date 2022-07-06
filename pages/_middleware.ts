c1e1d4aa8b6662a8ac184f305a3d37601e3a8685
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-06 19:04:42 +0100

    👽️ compat: NextJS 12.2.0 middleware update

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware({ cookies, nextUrl: { pathname, origin } }: NextRequest) {
-  if (cookies.account !== undefined || pathname === "/" || pathname.startsWith("/api/") || !pathname.endsWith("/"))
+  if (
+    cookies.get("account") !== undefined ||
+    pathname === "/" ||
+    pathname.startsWith("/api/") ||
+    !pathname.endsWith("/")
+  )
     return NextResponse.next();
   return NextResponse.redirect(`${origin}/?to=${encodeURIComponent(pathname)}`);
 }
