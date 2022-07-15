7e2755f8e12a8de4616b3a7821546e4406f384ce
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-15 11:14:34 +0100

    ✨ feat: add students codes page

---
+++ pages/codes.tsx
@@ -0,0 +1,19 @@
+import useSWR from "swr";
+
+import type { NextPage } from "next";
+
+const Codes: NextPage = () => {
+  const { data } = useSWR("/api/codes?filter=students");
+
+  return (
+    <div className="prose max-w-none w-full h-full">
+      <ul>
+        {data.students.map(({ _id, name, code }) => (
+          <li key={_id}>
+            {name.full} - {code}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
