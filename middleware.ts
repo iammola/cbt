bd317b0304f8e8ecd815ef1521cc3065ef287189
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-16 06:08:09 +0100

    🐛 fix(middleware): correctly redirect requests

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware({ cookies, nextUrl: { pathname, origin } }: NextRequest) {
-  if (
-    cookies.get("account") !== undefined ||
-    pathname === "/" ||
-    pathname.startsWith("/api/") ||
-    !pathname.endsWith("/")
-  )
-    return NextResponse.next();
-  return NextResponse.redirect(`${origin}/?to=${encodeURIComponent(pathname)}`);
+  /**
+   * Allow requests to the
+   * - index route - `/`
+   * - Next.js internals - `/_next/
+   * - API calls to ping and login - `/api/(login|ping)`
+   * - Public files - `\.(.+)`
+   */
+  if (/(^\/$|^\/(_next\/|api\/(login|ping))|\.(.+)$)/.test(pathname)) return NextResponse.next();
+
+  if (!cookies.get("account")) return NextResponse.redirect(`${origin}/?to=${encodeURIComponent(pathname)}`);
 }
