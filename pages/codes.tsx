7e109e0666785faf808f9f40d56f1bbccdfb30c0
Ademola Adedeji <a.mola.dev@gmail.com>
2022-07-16 04:39:11 +0100

    ✨ feat: group students by class and show fetching states

--- pages/codes.tsx
+++ pages/codes.tsx
@@ -1,21 +1,41 @@
 import useSWR from "swr";
+import { Fragment } from "react";
 
 import type { NextPage } from "next";
 import type { RouteData } from "types";
 import type { CodesGETData } from "types/api";
 
 const Codes: NextPage = () => {
-  const { data: { data } = {} } = useSWR<RouteData<CodesGETData>>("/api/codes");
+  const { data: { data } = {}, error } = useSWR<RouteData<CodesGETData>>("/api/codes");
 
   return (
-    <div className="prose prose-slate max-w-none w-full h-full p-10 text-sm tracking-wide">
-      <ul>
-        {data?.students.map(({ _id, name, code }) => (
-          <li key={_id}>
-            {name.full} - {code}
-          </li>
-        ))}
-      </ul>
+    <div className="prose prose-slate max-w-none w-full h-full p-5 md:p-10 text-sm tracking-wide">
+      <h2 className="text-center">Student Codes</h2>
+      {data ? (
+        Object.entries(
+          data.students
+            .sort((a, b) => a.academic[0].class.order - b.academic[0].class.order)
+            .reduce<{ [K: string]: CodesGETData["students"] }>((acc, cur) => {
+              const { name } = cur.academic[0].class;
+              return { ...acc, [name]: [...(acc[name] ?? []), cur] };
+            }, {})
+        ).map(([name, students]) => (
+          <Fragment key={name}>
+            <h4>{name}</h4>
+            <ul>
+              {students.map(({ _id, name, code }) => (
+                <li key={_id}>
+                  {name.full} - {code}
+                </li>
+              ))}
+            </ul>
+          </Fragment>
+        ))
+      ) : (
+        <div className="w-full text-center">
+          <h3>{error == null ? "Loading..." : "Error Fetching Codes"}</h3>
+        </div>
+      )}
     </div>
   );
 };
